6d328435fa1a0d2d43b28f821086062e15d532a2
Nayuki Minase <nayuki@eigenstate.org>
2010-09-19 19:18:38 +0000

    Added problem 2.

---
+++ src/p002.java
@@ -0,0 +1,29 @@
+public class p002 {
+	
+	public static void main(String[] args) {
+		int sum = 0;
+		for (int i = 0; ; i++) {
+			int fib = fibonacci(i);
+			if (fib > 4000000)
+				break;
+			if (fib % 2 == 0)
+				sum += fib;
+		}
+		System.out.println(sum);
+	}
+	
+	
+	private static int fibonacci(int x) {
+		if (x < 0 || x > 46)
+			throw new IllegalArgumentException();
+		int a = 0;
+		int b = 1;
+		for (int i = 0; i < x; i++) {
+			int c = a + b;
+			a = b;
+			b = c;
+		}
+		return a;
+	}
+	
+}
