15e3c29dd4d5e2526b210559ad06863cf108a494
Nayuki Minase <nayuki@eigenstate.org>
2010-09-19 20:51:03 +0000

    Added problem 40.

---
+++ p040.java
@@ -0,0 +1,28 @@
+public class p040 {
+	
+	public static void main(String[] args) {
+		StringBuilder sb = new StringBuilder();
+		for (int i = 1; i < 1000000; i++)
+			sb.append(i);
+		
+		System.out.println(
+			  getDigit(sb, 1)
+			* getDigit(sb, 10)
+			* getDigit(sb, 100)
+			* getDigit(sb, 1000)
+			* getDigit(sb, 10000)
+			* getDigit(sb, 100000)
+			* getDigit(sb, 1000000)
+		);
+	}
+	
+	
+	private static int getDigit(StringBuilder sb, int i) {
+		char c = sb.charAt(i - 1);
+		if (c >= '0' && c <= '9')
+			return c - '0';
+		else
+			throw new IllegalArgumentException();
+	}
+	
+}
