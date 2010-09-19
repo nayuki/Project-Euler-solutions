9e41f686701c72d6b42dfbd1543a2a4fbe3cf70c
Nayuki Minase <nayuki@eigenstate.org>
2010-09-19 19:16:32 +0000

    Added problem 1.

---
+++ src/p001.java
@@ -0,0 +1,12 @@
+public class p001 {
+	
+	public static void main(String[] args) {
+		int sum = 0;
+		for (int i = 0; i < 1000; i++) {
+			if (i % 3 == 0 || i % 5 == 0)
+				sum += i;
+		}
+		System.out.println(sum);
+	}
+
+}
