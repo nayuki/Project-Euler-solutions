7fce3880b4db24337a1e6a507b3687d747b6bacf
Nayuki Minase <nayuki@eigenstate.org>
2010-09-21 19:13:43 +0000

    Added problem 26.

---
+++ p026.java
@@ -0,0 +1,34 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class p026 {
+	
+	public static void main(String[] args) {
+		int bestNumber = -1;
+		int bestLength = -1;
+		
+		for (int i = 1; i <= 1000; i++) {
+			int len = getCycleLength(i);
+			if (bestLength == -1 || len > bestLength) {
+				bestNumber = i;
+				bestLength = len;
+			}
+		}
+		
+		System.out.println(bestNumber);
+	}
+	
+	
+	private static int getCycleLength(int n) {
+		Map<Integer,Integer> stateToIter = new HashMap<Integer,Integer>();
+		int state = 1;
+		int iter = 0;
+		while (!stateToIter.containsKey(state)) {
+			stateToIter.put(state, iter);
+			state = state * 10 % n;
+			iter++;
+		}
+		return iter - stateToIter.get(state);
+	}
+
+}
