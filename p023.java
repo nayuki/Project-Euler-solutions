20a28559ebd4735f86cb6dc8a2b9e0b9a3636f94
Nayuki Minase <nayuki@eigenstate.org>
2010-09-21 19:08:32 +0000

    Added problem 23.

---
+++ p023.java
@@ -0,0 +1,43 @@
+public class p023 {
+	
+	public static void main(String[] args) {
+		int sum = 0;
+		for (int i = 1; i <= 28123; i++) {
+			if (!isSumOf2Abundants(i))
+				sum += i;
+		}
+		System.out.println(sum);
+	}
+	
+	
+	private static boolean isSumOf2Abundants(int n) {
+		for (int i = 0; i <= n; i++) {
+			if (isAbundant[i] && isAbundant[n - i])
+				return true;
+		}
+		return false;
+	}
+	
+	
+	private static boolean[] isAbundant;
+	
+	static {
+		isAbundant = new boolean[28124];
+		for (int i = 1; i < isAbundant.length; i++)
+			isAbundant[i] = isAbundant(i);
+	}
+	
+	
+	private static boolean isAbundant(int n) {
+		if (n < 1)
+			throw new IllegalArgumentException();
+		
+		int sum = 0;
+		for (int i = 1; i < n; i++) {
+			if (n % i == 0)
+				sum += i;
+		}
+		return sum > n;
+	}
+	
+}
