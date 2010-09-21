ae4436225cba025d0a0ee8f462c2bc9db5e542cf
Nayuki Minase <nayuki@eigenstate.org>
2010-09-21 00:52:44 +0000

    Updated problem 206.

--- p206.java
+++ p206.java
@@ -1,17 +1,20 @@
-import java.math.BigInteger;
+import java.util.regex.Pattern;
 
 public class p206 {
 	
 	public static void main(String[] args) {
 		long i = 1000000000;
 		while (!isConcealedSquare(i))
-			i++;
+			i += 10;
 		System.out.println(i);
 	}
 	
 	
+	private static final Pattern PATTERN = Pattern.compile("1.2.3.4.5.6.7.8.9.0");
+	
+	
 	private static boolean isConcealedSquare(long n) {
-		return BigInteger.valueOf(n).pow(2).toString().matches("1.2.3.4.5.6.7.8.9.0");
+		return PATTERN.matcher(Long.toString(n * n)).matches();
 	}
 
 }
