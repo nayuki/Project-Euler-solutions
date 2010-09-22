663a6e9b4e256fb46b48f50ad7e78511ca41829d
Nayuki Minase <nayuki@eigenstate.org>
2010-09-22 03:50:12 +0000

    Added problem 28.

---
+++ p028.java
@@ -0,0 +1,13 @@
+public class p028 {
+	
+	public static void main(String[] args) {
+		int sum = 1;
+		for (int i = 0; i < 500; i++) {  // 500 rings
+			int n = i * 2 + 3;
+			int n2 = n * n;
+			sum += n2 + (n2 - (n - 1)) + (n2 - (n - 1) * 2) + (n2 - (n - 1) * 3);
+		}
+		System.out.println(sum);
+	}
+	
+}
