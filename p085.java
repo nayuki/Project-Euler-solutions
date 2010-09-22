860aca3c2a3c45da998ba29d54653187d186c968
Nayuki Minase <nayuki@eigenstate.org>
2010-09-22 05:31:29 +0000

    Added problem 85.

---
+++ p085.java
@@ -0,0 +1,23 @@
+public class p085 {
+	
+	public static void main(String[] args) {
+		int bestDiff = -1;
+		int bestArea = -1;
+		for (int w = 1; w < 2000; w++) {
+			for (int h = 1; h < 2000; h++) {
+				int diff = Math.abs(numberOfRectangles(w, h) - 2000000);
+				if (bestDiff == -1 || diff < bestDiff) {
+					bestDiff = diff;
+					bestArea = w * h;
+				}
+			}
+		}
+		System.out.println(bestArea);
+	}
+	
+	
+	private static int numberOfRectangles(int m, int n) {
+		return (m + 1) * m * (n + 1) * n / 4;
+	}
+	
+}
