b6ab86519565cfde96fd5761f60e32731a181d24
Nayuki Minase <nayuki@eigenstate.org>
2010-09-22 01:54:09 +0000

    Updated problem 214.

--- p214.java
+++ p214.java
@@ -2,16 +2,14 @@ public class p214 {
 	
 	public static void main(String[] args) {
 		int n = 40000000;
-		boolean[] isPrime = listPrimality(n + 1);
-		byte[] totientChainLength = new byte[n + 1];
+		boolean[] isPrime = listPrimality(n);
+		int[] totient = listTotients(n, isPrime);
+		
+		int[] totientChainLength = new int[n + 1];
 		
 		totientChainLength[0] = 0;
 		for (int i = 1; i < totientChainLength.length; i++) {
-			if(i%100000==0)System.out.println(i);
-			int val = totientChainLength[totient(i)] + 1;
-			if (val < 0)
-				throw new ArithmeticException("Overflow");
-			totientChainLength[i] = (byte)val;
+			totientChainLength[i] = totientChainLength[totient[i]] + 1;
 		}
 		
 		long sum = 0;
@@ -41,24 +39,30 @@ public class p214 {
 	}
 	
 	
-	private static int totient(int x) {
-		if (x <= 0)
-			throw new IllegalArgumentException("Totient of non-positive integer");
-		int p = 1;
-		for (int i = 2, end = sqrt(x); i <= end; i++) {
-			if (x % i == 0) {
-				p *= i - 1;
-				x /= i;
-				while (x % i == 0) {
-					p *= i;
-					x /= i;
+	private static int[] listTotients(int n, boolean[] isPrime) {
+		int[] totients = new int[n + 1];
+		totients[1] = 1;
+		for (int i = 2; i <= n; i++) {
+			if (isPrime[i])
+				totients[i] = i - 1;
+			else {
+				for (int j = 2; ; j++) {  // This loop will terminate before j > sqrt(n)
+					if (isPrime[j] && i % j == 0) {  // Only need to consider prime factors
+						int tot = j - 1;
+						int temp = i / j;
+						while (temp % j == 0) {
+							tot *= j;
+							temp /= j;
+						}
+						// temp is now coprime to j
+						tot *= totients[temp];
+						totients[i] = tot;
+						break;
+					}
 				}
-				end = sqrt(x);
 			}
 		}
-		if (x != 1)
-			p *= x - 1;
-		return p;
+		return totients;
 	}
 	
 	
