0c5aa4d51bcf749f1015822aac382d3df81855a1
Nayuki Minase <nayuki@eigenstate.org>
2010-09-22 14:18:36 +0000

    Updated problem 160.

--- p160.java
+++ p160.java
@@ -1,19 +1,22 @@
 public class p160 {
 	
 	public static void main(String[] args) {
+		System.out.println(factorialLast5Digits(1000000000000L));
+	}
+	
+	
+	private static int factorialLast5Digits(long n) {
 		long x = 1;
 		long twos = 0;
-		for (int i = 1; i <= 1000000000; i++) {
-			int j = i;
+		for (long i = 1; i <= n; i++) {
+			long j = i;
 			for (; j % 2 == 0; twos++, j /= 2);
 			for (; j % 5 == 0; twos--, j /= 5);
 			
 			x = x * j % 100000;
 		}
-		
 		for (; twos > 0; x = x * 2 % 100000, twos--);
-		
-		System.out.println(x);
+		return (int)x;
 	}
 	
 }
