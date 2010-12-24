4406d6b2d80ab03a409093ab3290357ae7b6e863
Nayuki Minase <nayuki@eigenstate.org>
2010-12-24 20:23:54 +0000

    Added problem 44.

---
+++ p044.java
@@ -0,0 +1,34 @@
+public class p044 {
+	
+	public static void main(String[] args) {
+		outer:
+		for (long i = 1; ; i++) {
+			long penti = pentagonalNumber(i);
+			for (long j = i; pentagonalNumber(j) - pentagonalNumber(j - 1) <= penti; j++) {
+				if (isPentagonalNumber(pentagonalNumber(j) - penti) && isPentagonalNumber(2 * pentagonalNumber(j) - penti)) {
+					System.out.println(penti);
+					break outer;
+				}
+			}
+		}
+	}
+	
+	
+	private static long pentagonalNumber(long n) {
+		if (n <= 0 || n > 2479700524L)
+			throw new IllegalArgumentException();
+		return n * (3 * n - 1) >>> 1;
+	}
+	
+	
+	private static boolean isPentagonalNumber(long n) {
+		long temp = 0;
+		for (long i = 2147483648L; i != 0; i >>>= 1) {
+			temp |= i;
+			if (temp > 2479700524L || pentagonalNumber(temp) > n)
+				temp ^= i;
+		}
+		return temp != 0 && pentagonalNumber(temp) == n;
+	}
+	
+}
