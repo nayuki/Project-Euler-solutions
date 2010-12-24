d282939422fbbe005c7a5ba44b4712a19379ae23
Nayuki Minase <nayuki@eigenstate.org>
2010-12-24 22:21:38 +0000

    Added problem 145.

---
+++ p145.java
@@ -0,0 +1,32 @@
+public class p145 {
+	
+	public static void main(String[] args) {
+		int count = 0;
+		for (int i = 0; i < 1000000000; i++) {
+			if (isReversible(i))
+				count++;
+		}
+		System.out.println(count);
+	}
+	
+	
+	private static boolean isReversible(int x) {
+		return x % 10 != 0 && hasOnlyOddDigits(x + reverse(x));
+	}
+	
+	
+	private static int reverse(int x) {
+		return Integer.parseInt(new StringBuffer().append(x).reverse().toString());
+	}
+	
+	
+	private static boolean hasOnlyOddDigits(int x) {
+		while (x != 0) {
+			if (x % 2 == 0)
+				return false;
+			x /= 10;
+		}
+		return true;
+	}
+	
+}
