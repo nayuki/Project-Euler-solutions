89147a033b85a6f7caa71977d5f414ffe3b1ec9b
Nayuki Minase <nayuki@eigenstate.org>
2011-06-13 04:47:23 +0000

    Added problem 49.

---
+++ p049.java
@@ -0,0 +1,33 @@
+import java.util.Arrays;
+
+public class p049 {
+	
+	private static boolean[] isPrime = Library.listPrimality(10000);
+	
+	
+	public static void main(String[] args) {
+		for (int base = 1000; base < 10000; base++) {
+			if (!isPrime[base])
+				continue;
+			middle:
+			for (int stepSize = 1; stepSize < 10000; stepSize++) {
+				for (int i = 1; i <= 2; i++) {
+					int n = base + i * stepSize;
+					if (n >= 10000 || !isPrime[n] || !hasSameDigits(base, n))
+						continue middle;
+				}
+				System.out.println(base + " " + (base + stepSize) + " " + (base + stepSize * 2));
+			}
+		}
+	}
+	
+	
+	private static boolean hasSameDigits(int x, int y) {
+		char[] xdigits = Integer.toString(x).toCharArray();
+		char[] ydigits = Integer.toString(y).toCharArray();
+		Arrays.sort(xdigits);
+		Arrays.sort(ydigits);
+		return Arrays.equals(xdigits, ydigits);
+	}
+	
+}
