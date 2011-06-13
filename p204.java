48e1553b811e125f8b1239a61a47f51be58ab4df
Nayuki Minase <nayuki@eigenstate.org>
2011-06-13 18:47:54 +0000

    Updated problem 204.

--- p204.java
+++ p204.java
@@ -1,24 +1,27 @@
 public class p204 {
 	
 	public static void main(String[] args) {
-		int count = 0;
-		for (int i = 1; i <= 1000000000; i++) {
-			if (isGeneralizedHamming100(i))
-				count++;
-		}
-		System.out.println(count);
+		System.out.println(count(0, 1));
 	}
 	
+	
+	private static long LIMIT = 1000000000;
 	
 	private static int[] primes = {2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97};
 	
 	
-	private static boolean isGeneralizedHamming100(int n) {
-		for (int p : primes) {
-			while (n % p == 0)
-				n /= p;
+	private static int count(int primeIndex, long product) {
+		if (primeIndex == primes.length)
+			return product <= LIMIT ? 1 : 0;
+		
+		else {
+			int count = 0;
+			while (product <= LIMIT) {
+				count += count(primeIndex + 1, product);
+				product *= primes[primeIndex];
+			}
+			return count;
 		}
-		return n == 1;
 	}
 	
 }
