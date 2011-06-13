1a02f19ea6a930cd43f1dba0be83c26f4f0fe8a3
Nayuki Minase <nayuki@eigenstate.org>
2011-06-13 05:10:39 +0000

    Added problem 92.

---
+++ p092.java
@@ -0,0 +1,33 @@
+public class p092 {
+	
+	public static void main(String[] args) {
+		int count = 0;
+		for (int i = 1; i < 10000000; i++) {
+			if (isClass89(i))
+				count++;
+		}
+		System.out.println(count);
+	}
+	
+	
+	private static boolean isClass89(int x) {
+		while (true) {
+			switch (x) {
+				case  1:  return false;
+				case 89:  return true;
+				default:  x = nextNumber(x);
+			}
+		}
+	}
+	
+	
+	private static int nextNumber(int x) {
+		int sum = 0;
+		while (x != 0) {
+			sum += (x % 10) * (x % 10);
+			x /= 10;
+		}
+		return sum;
+	}
+	
+}
