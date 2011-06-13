ae1c35dc256a055b302f282761d4c64b57bd6bd3
Nayuki Minase <nayuki@eigenstate.org>
2011-06-13 21:20:00 +0000

    Added problem 117.

---
+++ p117.java
@@ -0,0 +1,14 @@
+public class p117 {
+	
+	public static void main(String[] args) {
+		long[] ways = new long[51];  // Memoization
+		ways[0] = 1;
+		ways[1] = 1;
+		for (int i = 2; i <= 50; i++) {
+			for (int j = 1; j <= 4 && j <= i; j++)
+				ways[i] += ways[i - j];  // Dynamic programming
+		}
+		System.out.println(ways[50]);
+	}
+	
+}
