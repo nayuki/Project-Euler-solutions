ef2b570ffbe82d38390b0d6a16b791164c9b9be4
Nayuki Minase <nayuki@eigenstate.org>
2011-06-13 18:12:07 +0000

    Added problem 70.

---
+++ p070.java
@@ -0,0 +1,28 @@
+import java.util.Arrays;
+
+
+public class p070 {
+	
+	public static void main(String[] args) {
+		int minNumerator = 10;
+		int minDenominator = 1;
+		for (int i = 2; i < 10000000; i++) {
+			int tot = Library.totient(i);
+			if (hasSameDigits(i, tot) && (long)i * minDenominator < (long)minNumerator * tot) {
+				minNumerator = i;
+				minDenominator = tot;
+			}
+		}
+		System.out.println(minNumerator);
+	}
+	
+	
+	private static boolean hasSameDigits(int x, int y) {
+		char[] xdigits = Integer.toString(x).toCharArray();
+		char[] ydigits = Integer.toString(y).toCharArray();
+		Arrays.sort(xdigits);
+		Arrays.sort(ydigits);
+		return Arrays.equals(xdigits, ydigits);
+	}
+	
+}
