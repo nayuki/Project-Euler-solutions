8d0ee551faa3d2d268d24cb98ec47cce4815762a
Nayuki Minase <nayuki@eigenstate.org>
2011-06-14 23:20:31 +0000

    Added problem 9, Haskell version.

---
+++ p009.hs
@@ -0,0 +1,7 @@
+import Char (ord)
+
+cond a b = a <= b && b <= c
+        && a * a + b * b == c * c
+        where c = 1000 - a - b
+ans = [a * b * (1000 - a - b) | a <- [1..1000], b <- [1..1000], cond a b]
+main = putStrLn (show ans)
