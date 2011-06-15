7ab42a3643f6ba2eee0ee27a010322c2206035e7
Nayuki Minase <nayuki@eigenstate.org>
2011-06-15 02:13:57 +0000

    Added problem 115.

---
+++ p115.java
@@ -0,0 +1,26 @@
+public class p115 {
+	
+	public static void main(String[] args) {
+		for (int i = 1; ; i++) {
+			if (ways(50, i) > 1000000) {
+				System.out.println(i);
+				break;
+			}
+		}
+	}
+	
+	
+	private static long ways(int m, int n) {
+		long[] ways = new long[n + 1];  // Memoization
+		ways[0] = 1;
+		for (int i = 1; i <= n; i++) {
+			ways[i] += ways[i - 1];
+			for (int j = m; j < i; j++)
+				ways[i] += ways[i - j - 1];  // Dynamic programming
+			if (i >= m)
+				ways[i] += ways[0];
+		}
+		return ways[n];
+	}
+	
+}
