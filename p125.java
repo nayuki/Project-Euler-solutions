01e5248c4b87e45c0afc827c5672157006a9b82c
Nayuki Minase <nayuki@eigenstate.org>
2012-02-12 18:40:39 +0000

    P125: Added Java solution.

---
+++ p125.java
@@ -0,0 +1,38 @@
+/* 
+ * Solution to Project Euler problem 125
+ * By Nayuki Minase
+ */
+
+import java.util.HashSet;
+import java.util.Set;
+
+
+public class p125 {
+	
+	public static void main(String[] args) {
+		Set<Integer> nums = new HashSet<Integer>();;
+		for (int i = 1; i <= 10000; i++) {
+			int sum = i * i;
+			for (int j = i + 1; ; j++) {
+				sum += j * j;
+				if (sum >= 100000000)
+					break;
+				if (isPalindrome(sum))
+					nums.add(sum);
+			}
+		}
+		
+		long sum = 0;
+		for (int x : nums)
+			sum += x;
+		System.out.println(sum);
+	}
+	
+	
+	private static boolean isPalindrome(int x) {
+		String s = Integer.toString(x);
+		String rev = new StringBuilder(s).reverse().toString();
+		return s.equals(rev);
+	}
+	
+}
