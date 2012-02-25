9eb8e3b6e188161c12ff00ff369773b01cb00a6d
Nayuki Minase <nayuki@eigenstate.org>
2012-02-25 16:22:05 +0000

    P87: Added Java solution.

---
+++ p087.java
@@ -0,0 +1,50 @@
+/* 
+ * Solution to Project Euler problem 87
+ * By Nayuki Minase
+ */
+
+
+public class p087 {
+	
+	public static void main(String[] args) {
+		boolean[] isPrime = Library.listPrimality(7071);  // sqrt(50 000 000)
+		
+		// Squares
+		boolean[] possible = new boolean[50000000 + 1];
+		for (int i = 0; i < isPrime.length && i * i < possible.length; i++) {
+			if (!isPrime[i])
+				continue;
+			possible[i * i] = true;
+		}
+		
+		// Cubes
+		boolean[] nextPossible = new boolean[possible.length];
+		for (int i = 0; i < isPrime.length && (long)i * i * i < possible.length; i++) {
+			if (!isPrime[i])
+				continue;
+			int prod = i * i * i;
+			for (int j = possible.length - 1 - prod; j >= 0; j--)
+				nextPossible[j + prod] |= possible[j];
+		}
+		possible = nextPossible;
+		
+		// Fourth power
+		nextPossible = new boolean[possible.length];
+		for (int i = 0; i < isPrime.length && (long)i * i * i * i < possible.length; i++) {
+			if (!isPrime[i])
+				continue;
+			int prod = i * i * i * i;
+			for (int j = possible.length - 1 - prod; j >= 0; j--)
+				nextPossible[j + prod] |= possible[j];
+		}
+		possible = nextPossible;
+		
+		int count = 0;
+		for (boolean x : possible) {
+			if (x)
+				count++;
+		}
+		System.out.println(count);
+	}
+	
+}
