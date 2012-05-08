615a56988c78c12678506fe24447655f20274791
Nayuki Minase <nayuki@eigenstate.org>
2012-05-08 17:40:58 +0000

    P243: Added Java solution.

---
+++ p243.java
@@ -0,0 +1,46 @@
+/* 
+ * Solution to Project Euler problem 243
+ * By Nayuki Minase
+ */
+
+import java.math.BigInteger;
+
+
+public class p243 {
+	
+	private static BigInteger TARGET_NUMERATOR   = BigInteger.valueOf(15499);
+	private static BigInteger TARGET_DENOMINATOR = BigInteger.valueOf(94744);
+	
+	
+	public static void main(String[] args) {
+		BigInteger numer = BigInteger.ONE;
+		BigInteger denom = BigInteger.ONE;
+		
+		// This is related to computing totients.
+		// To make the totient smaller relative to the number, we must add new prime factors.
+		for (int p = 2; ; p = nextPrime(p)) {
+			numer = numer.multiply(BigInteger.valueOf(p - 1));
+			denom = denom.multiply(BigInteger.valueOf(p));
+			if (numer.multiply(TARGET_DENOMINATOR).compareTo(denom.multiply(TARGET_NUMERATOR)) < 0) {
+				for (int i = 1; i <= p; i++) {
+					BigInteger n = numer.multiply(BigInteger.valueOf(i));
+					BigInteger d = denom.multiply(BigInteger.valueOf(i)).subtract(BigInteger.ONE);
+					if (n.multiply(TARGET_DENOMINATOR).compareTo(d.multiply(TARGET_NUMERATOR)) < 0) {
+						System.out.println(d.add(BigInteger.ONE));
+						return;
+					}
+				}
+			}
+		}
+	}
+	
+	
+	private static int nextPrime(int n) {
+		if (n < 0 || n == 2147483647)
+			throw new IllegalArgumentException();
+		do n++;
+		while (!Library.isPrime(n));
+		return n;
+	}
+	
+}
