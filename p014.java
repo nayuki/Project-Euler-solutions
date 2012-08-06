c37c3c45560110254a1e301e0d169905468e7678
Nayuki Minase <nayuki@eigenstate.org>
2012-08-06 20:40:32 +0000

    P14: Simplified code by de-optimizing.

--- p014.java
+++ p014.java
@@ -3,68 +3,41 @@
  * By Nayuki Minase
  */
 
-import java.util.Arrays;
+import java.math.BigInteger;
 
 
 public class p014 {
 	
 	public static void main(String[] args) {
-		int maxArg = 1;
-		int maxVal = 0;
+		int maxArg = -1;
+		int maxChain = 0;
 		for (int i = 1; i < 1000000; i++) {
-			if (collatz(i) > maxVal) {
+			int chainLen = collatzChainLength(BigInteger.valueOf(i));
+			if (chainLen > maxChain) {
 				maxArg = i;
-				maxVal = collatz(i);
+				maxChain = chainLen;
 			}
 		}
-		System.out.println(maxArg + " " + maxVal);
+		System.out.println(maxArg + " " + maxChain);
 	}
 	
 	
-	
-	private static int[] collatz;  // Memoization
-	
-	static {
-		collatz = new int[10000000];
-		Arrays.fill(collatz, -1);
-		collatz[1] = 1;
-	}
-	
-	
-	private static int collatz(long n) {
-		if (n <= 0)
+	private static int collatzChainLength(BigInteger n) {
+		if (n.signum() < 0)
 			throw new IllegalArgumentException();
 		
-		if (n < collatz.length && collatz[(int)n] != -1)
-			return collatz[(int)n];  // Return memoized
-		else {
-			int result;
-			if (n % 2 == 0) result = collatz(n / 2) + 1;
-			else            result = collatz(checkedAdd(checkedMultiply(n, 3), 1)) + 1;
-			
-			if (n < collatz.length) collatz[(int)n] = result;  // Memoize
-			return result;
+		int count = 1;
+		while (true) {
+			if (n.equals(BigInteger.ONE))
+				return count;
+			else {
+				if (!n.testBit(0))  // Test if it's even
+					n = n.shiftRight(1);
+				else
+					n = n.multiply(BigInteger.valueOf(3)).add(BigInteger.ONE);
+				count++;
+			}
 		}
-	}
-	
-	
-	private static long checkedAdd(long x, long y) {
-		long z = x + y;
-		if (y > 0 && z < x || y < 0 && z > x)
-			throw new ArithmeticException();
-		else
-			return z;
-	}
-	
-	
-	private static long checkedMultiply(long x, long y) {
-		if (x <= 0 || y <= 0)
-			throw new UnsupportedOperationException("Not implemented");
-		
-		if (x <= Long.MAX_VALUE / y)
-			return x * y;
-		else
-			throw new ArithmeticException();
 	}
 	
 }
