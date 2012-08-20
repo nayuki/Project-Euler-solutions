a3e74465c7268a275596d769959e1197cd902425
Nayuki Minase <nayuki@eigenstate.org>
2012-08-20 23:43:59 +0000

    P63, P381: Clarified solution.

--- p381.java
+++ p381.java
@@ -20,14 +20,18 @@ public class p381 {
 	/*
 	 * Note about the mathematical simplification:
 	 * (p-5)! + (p-4)! + (p-3)! + (p-2)! + (p-1)!
-	 * = (p-5)! (1 + (p-4) + (p-4)(p-3) + (p-4)(p-3)(p-2) + (p-4)(p-3)(p-2)(p-1))
-	 * = (p-5)! (1 + -4 + 12 + -24 + 24)
+	 * = (p-5)! * (1 + (p-4) + (p-4)(p-3) + (p-4)(p-3)(p-2) + (p-4)(p-3)(p-2)(p-1))
+	 * = (p-5)! * (1 + (-4) + (-4)(-3) + (-4)(-3)(-2) + (-4)(-3)(-2)(-1))
+	 * = (p-5)! * (1 + -4 + 12 + -24 + 24)
 	 * = (p-5)! * 9
 	 * = (p-1)! / ((p-1)(p-2)(p-3)(p-4)) * 9
+	 * = (p-1)! / ((-1)(-2)(-3)(-4)) * 9
 	 * = (p-1)! / 24 * 9
+	 * = (p-1)! * (3 * 3) / (3 * 8)
 	 * = (p-1)! * 3 / 8
 	 * = -1 * 3 / 8  (by Wilson's theorem)
-	 * = -3/8 mod p
+	 * = -3/8 mod p.
+	 * Every part of the equation is modulo a prime p > 4.
 	 */
 	private static int s(int p) {
 		return (int)((long)(p - 3) * reciprocalMod(8 % p, p) % p);
