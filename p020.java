b40548fa953086c094d714956d8b25f9f235854a
Nayuki Minase <nayuki@eigenstate.org>
2012-10-04 01:22:42 +0000

    P20: Made use of library function.

--- p020.java
+++ p020.java
@@ -3,25 +3,15 @@
  * By Nayuki Minase
  */
 
-import java.math.BigInteger;
-
 
 public class p020 {
 	
 	public static void main(String[] args) {
-		String temp = factorial(100).toString();
+		String temp = Library.factorial(100).toString();
 		int sum = 0;
 		for (int i = 0; i < temp.length(); i++)
 			sum += temp.charAt(i) - '0';
 		System.out.println(sum);
-	}
-	
-	
-	private static BigInteger factorial(int n) {
-		BigInteger prod = BigInteger.ONE;
-		for (int i = 1; i <= n; i++)
-			prod = prod.multiply(BigInteger.valueOf(i));
-		return prod;
 	}
 	
 }
