d4125093dbf455a82406069c2a3dcd874c9a0113
Nayuki Minase <nayuki@eigenstate.org>
2012-10-23 17:07:29 +0000

    P249: Added Java solution.

---
+++ p249.java
@@ -0,0 +1,38 @@
+/* 
+ * Solution to Project Euler problem 249
+ * By Nayuki Minase
+ * 
+ * http://nayuki.eigenstate.org/page/project-euler-solutions
+ * https://github.com/nayuki/Project-Euler-solutions
+ */
+
+
+public class p249 {
+	
+	private static int LIMIT = 5000;
+	private static long MODULUS = 10000000000000000L;
+	
+	
+	public static void main(String[] args) {
+		boolean[] isPrime = Library.listPrimality(LIMIT * LIMIT / 2);
+		long[] numSubsets = new long[LIMIT * LIMIT / 2];  // numSubsets[i] is the number of subsets with sum i, mod 10^16
+		numSubsets[0] = 1;
+		int maxIndex = 0;
+		
+		for (int i = 0; i < LIMIT; i++) {
+			if (!isPrime[i])
+				continue;
+			maxIndex += i;
+			for (int j = maxIndex; j >= i; j--)
+				numSubsets[j] = (numSubsets[j] + numSubsets[j - i]) % MODULUS;
+		}
+		
+		long sum = 0;
+		for (int i = 0; i < numSubsets.length; i++) {
+			if (isPrime[i])
+				sum = (sum + numSubsets[i]) % MODULUS;
+		}
+		System.out.println(sum);
+	}
+	
+}
