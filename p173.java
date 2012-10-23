1fa0dd1a7d93a4b5fa179e42fedf14d986c4ad8e
Nayuki Minase <nayuki@eigenstate.org>
2012-10-23 03:17:22 +0000

    P173: Added comments.

--- p173.java
+++ p173.java
@@ -11,8 +11,8 @@ public class p173 {
 	
 	public static void main(String[] args) {
 		int count = 0;
-		for (int a = 3; a <= 1000000; a ++) {
-			for (int b = a - 2; b >= 1; b -= 2) {
+		for (int a = 3; a <= 1000000; a++) {  // Outer square length
+			for (int b = a - 2; b >= 1; b -= 2) {  // Inner square length
 				long tiles = (long)a * a - (long)b * b;
 				if (tiles > 1000000)
 					break;
