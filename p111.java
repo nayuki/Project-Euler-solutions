86f7cc48f5f05cbf9fb4f104f54c509643074c72
Nayuki Minase <nayuki@eigenstate.org>
2012-10-24 13:58:04 +0000

    P111: Optimized Java solution using block-based sieve of Eratosthenes.

--- p111.java
+++ p111.java
@@ -6,11 +6,12 @@
  * https://github.com/nayuki/Project-Euler-solutions
  */
 
+import java.util.Arrays;
+
 
 public class p111 {
 	
 	private static int[] primes;
-	private static long[] primesSquared;
 	
 	static {
 		boolean[] isPrime = Library.listPrimality((int)Library.sqrt(9999999999L));
@@ -20,25 +21,28 @@ public class p111 {
 				count++;
 		}
 		primes = new int[count];
-		primesSquared = new long[count];
 		for (int i = 0, j = 0; i < isPrime.length; i++) {
 			if (isPrime[i]) {
 				primes[j] = i;
-				primesSquared[j] = (long)i * i;
 				j++;
 			}
 		}
 	}
 	
 	
+	private static final int BLOCK_SIZE = 10000000;
+	
 	public static void main(String[] args) {
 		// Classify each 9-digit prime number for each digit value
 		long[][] classSums = new long[10][11];  // classSums[i][j] is the sum of all the prime numbers where digit i occurs exactly j times
-		for (long i = 1000000001; i <= 9999999999L; i += 2) {
-			if (isPrime(i)) {
-				int[] digitCounts = getDigitCounts(i);
-				for (int j = 0; j < digitCounts.length; j++)
-					classSums[j][digitCounts[j]] += i;
+		for (long i = 1000000000; i < 10000000000L; i += BLOCK_SIZE) {
+			boolean[] isPrime = sievePrimes(i, BLOCK_SIZE);
+			for (int j = 0; j < isPrime.length; j++) {
+				if (isPrime[j]) {
+					int[] digitCounts = getDigitCounts(i + j);
+					for (int k = 0; k < digitCounts.length; k++)
+						classSums[k][digitCounts[k]] += i + j;
+				}
 			}
 		}
 		
@@ -55,21 +59,23 @@ public class p111 {
 	}
 	
 	
+	private static boolean[] sievePrimes(long start, int n) {
+		boolean[] isPrime = new boolean[n];
+		Arrays.fill(isPrime, true);
+		for (int p : primes) {
+			for (int i = (p - (int)(start % p)) % p; i < isPrime.length; i += p)
+				isPrime[i] = false;
+		}
+		return isPrime;
+	}
+	
+	
 	// Returns the number of times each decimal digit occurred, excluding leading zeros.
 	private static int[] getDigitCounts(long n) {
 		int[] result = new int[10];
 		for (; n != 0; n /= 10)
 			result[(int)(n % 10)]++;
 		return result;
-	}
-	
-	
-	private static boolean isPrime(long x) {
-		for (int i = 0; i < primes.length && primesSquared[i] <= x; i++) {
-			if (x % primes[i] == 0)
-				return false;
-		}
-		return true;
 	}
 	
 }
