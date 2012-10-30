38981a97dd0152bac597bab6c5dd8d9c3652ac17
Nayuki Minase <nayuki@eigenstate.org>
2012-10-30 02:54:15 +0000

    P44: Vastly sped up Java solution.

--- p044.java
+++ p044.java
@@ -15,32 +15,45 @@ public final class p044 {
 	
 	
 	public String run() {
-		for (long i = 1; ; i++) {  // For each pentagonal difference
+		long minD = -1;
+		for (int i = 2; ; i++) {  // For each upper pentagonal number index, going upward
 			long penti = pentagonalNumber(i);
-			for (long j = 1; ; j++) {  // For each lower pentagonal number
+			if (minD != -1 && penti - pentagonalNumber(i - 1) > minD)  // If the next number down is more than a found difference, then conclude searching
+				break;
+			for (int j = i - 1; j >= 1; j--) {  // For each lower pentagonal number index, going downward
 				long pentj = pentagonalNumber(j);
-				if (pentagonalNumber(j + 1) - pentj > penti)
+				long diff = penti - pentj;
+				if (minD != -1 && diff >= minD)  // If the difference is at least as great as a found difference, then stop testing lower pentagonal numbers
 					break;
-				if (isPentagonalNumber(pentj + penti) && isPentagonalNumber(pentj * 2 + penti))
-					return Long.toString(penti);
+				else if (isPentagonalNumber(penti + pentj) && isPentagonalNumber(diff) && (minD == -1 || diff < minD))
+					minD = diff;
 			}
 		}
+		return Long.toString(minD);
 	}
 	
 	
-	private static long pentagonalNumber(long n) {
-		if (n <= 0 || n > 2479700524L)
+	private static long pentagonalNumber(int x) {
+		if (x <= 0)
 			throw new IllegalArgumentException();
-		return n * (3 * n - 1) >>> 1;
+		return (long)x * (x * 3 - 1) >>> 1;
 	}
 	
 	
-	private static boolean isPentagonalNumber(long n) {
-		if (n <= 0)
+	private static boolean isPentagonalNumber(long y) {
+		if (y <= 0)
 			return false;
-		long temp = n * 24 + 1;
+		
+		/* 
+		 * If y = pentagonalNumber(x) = x (3x-1) / 2,
+		 * then by the quadratic formula, the positive solution is x = (sqrt(24y + 1) + 1) / 6.
+		 * There exists a solution for x if and only if both of these conditions hold:
+		 * (24y + 1) is a perfect square, and sqrt(24y + 1) + 1 mod 6 = 0.
+		 * The second condition is equivalent to sqrt(24y + 1) = 5 mod 6.
+		 */
+		long temp = y * 24 + 1;
 		long sqrt = Library.sqrt(temp);
-		return sqrt * sqrt == temp && (sqrt + 1) % 6 == 0;
+		return sqrt * sqrt == temp && sqrt % 6 == 5;
 	}
 	
 }
