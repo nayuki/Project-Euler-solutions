98ad81ba5db1299ab5265b3b399a863f8df86ea4
Nayuki Minase <nayuki@eigenstate.org>
2012-10-30 04:04:54 +0000

    P79: Sped up Java solution.

--- p079.java
+++ p079.java
@@ -14,46 +14,59 @@ public final class p079 implements EulerSolution {
 	}
 	
 	
-	private static String[] SUBSEQS = {"319", "680", "180", "690", "129", "620", "762", "689", "762", "318", "368", "710", "720", "710", "629", "168", "160", "689", "716", "731", "736", "729", "316", "729", "729", "710", "769", "290", "719", "680", "318", "389", "162", "289", "162", "718", "729", "319", "790", "680", "890", "362", "319", "760", "316", "729", "380", "319", "728", "716"};
+	private String[] SUBSEQS = {"319", "680", "180", "690", "129", "620", "762", "689", "762", "318", "368", "710", "720", "710", "629", "168", "160", "689", "716", "731", "736", "729", "316", "729", "729", "710", "769", "290", "719", "680", "318", "389", "162", "289", "162", "718", "729", "319", "790", "680", "890", "362", "319", "760", "316", "729", "380", "319", "728", "716"};
+	
+	private char[] packedSubseqs;
 	
 	
 	public String run() {
-		for (int len = 3; ; len++) {
+		// Preprocessing
+		packedSubseqs = new char[SUBSEQS.length * 3];
+		for (int i = 0; i < SUBSEQS.length; i++) {
+			for (int j = 0; j < 3; j++)
+				packedSubseqs[i * 3 + j] = SUBSEQS[i].charAt(j);
+		}
+		
+		// Try ascending lengths
+		for (int len = 3; len <= 10; len++) {
 			int end = Library.pow(10, len);
 			for (int guess = 0; guess < end; guess++) {
-				if (isConsistent(toString(guess, len)))
-					return toString(guess, len);
+				char[] guessChars = toChars(guess, len);
+				if (isConsistent(guessChars))
+					return new String(guessChars);
 			}
 		}
+		throw new RuntimeException("Not found");
 	}
 	
 	
-	private static boolean isConsistent(String guess) {
-		for (String subseq : SUBSEQS) {
-			if (!isSubsequence(guess, subseq))
+	private boolean isConsistent(char[] guess) {
+		// For each string 's' in SUBSEQS, test if 's' is a subsequence of 'guess'
+		for (int i = 0; i < packedSubseqs.length; i += 3) {
+			int j = 0;  // Index in 's'
+			for (int k = 0; k < guess.length && j < 3; k++) {  // Index in 'guess'
+				if (guess[k] == packedSubseqs[i + j])
+					j++;
+			}
+			if (j < 3)  // Not all characters consumed, fail
 				return false;
 		}
 		return true;
 	}
 	
 	
-	private static boolean isSubsequence(String parent, String child) {
-		int j = 0;
-		for (int i = 0; i < parent.length() && j < child.length(); i++) {
-			if (parent.charAt(i) == child.charAt(j))
-				j++;
-		}
-		return j == child.length();
-	}
-	
-	
-	private static String toString(int n, int len) {
-		String s = Integer.toString(n);
-		if (s.length() > len)
+	// Converts integer to string with zero padding, in little endian.
+	// Since we're trying all combinations, the order doesn't matter.
+	private static char[] toChars(int n, int len) {
+		char[] result = new char[len];
+		int i = 0;
+		for (; i < result.length; i++, n /= 10)
+			result[i] = (char)('0' + (n % 10));
+		if (n != 0)
 			throw new IllegalArgumentException();
-		while (s.length() < len)
-			s = "0" + s;
-		return s;
+		for (; i < result.length; i++)
+			result[i] = '0';
+		return result;
 	}
 	
 }
