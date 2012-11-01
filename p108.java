3cd327a4434156eec41da65f8a45702947633f41
Nayuki Minase <nayuki@eigenstate.org>
2012-11-01 23:25:39 +0000

    P108: Rewrote Java solution with faster algorithm, added explanation.

--- p108.java
+++ p108.java
@@ -14,20 +14,45 @@ public final class p108 implements EulerSolution {
 	}
 	
 	
+	/* 
+	 * Rewrite the equation with x = n+i, y = n+j, and manipulate it:
+	 *   1/n = 1/x + 1/y
+	 *       = 1/(n+i) + 1/(n+j)
+	 *       = (2n+i+j) / ((n+i)(n+j)).
+	 *   n(2n+i+j) = (n+i)(n+j).
+	 *   2n^2 + ni + nj = n^2 + ni + nj + ij.
+	 *   n^2 = ij.
+	 * Hence i and j are divisors of n^2. To ensure unique solutions,
+	 * we impose that x <= y, so i <= j. Also, i > 0, otherwise no j exists.
+	 * We have i <= j = n^2 / i, thus i^2 <= n^2. With i being positive, we get that i <= n.
+	 * Therefore the number of solutions for i is the number of divisors of n^2 in the range [1, n].
+	 * n^2 always has an odd number of divisors. One of them is n. As for the remainder of them, half of them are below n
+	 * and half of them are above n. So if n^2 has m divisors, then we want (m+1)/2 of them as solutions for i.
+	 */
 	public String run() {
-		int n = 1;
-		while (numberOfSolutions(n) <= 1000)
-			n++;
-		return Integer.toString(n);
+		for (int n = 1; ; n++) {
+			if ((countDivisorsSquared(n) + 1) / 2 > 1000)
+				return Integer.toString(n);
+		}
 	}
 	
 	
-	private static int numberOfSolutions(int n) {
-		int count = 0;
-		for (int x = n + 1; x <= 2 * n; x++) {
-			if ((long)x * n % (x - n) == 0)
-				count++;
+	// Returns the number of divisors of n^2
+	private static int countDivisorsSquared(int n) {
+		int count = 1;
+		for (int i = 2, end = Library.sqrt(n); i <= end; i++) {
+			if (n % i == 0) {
+				int j = 0;
+				do {
+					n /= i;
+					j++;
+				} while (n % i == 0);
+				count *= j * 2 + 1;
+				end = Library.sqrt(n);
+			}
 		}
+		if (n != 1)  // Remaining largest prime factor
+			count *= 3;
 		return count;
 	}
 	
