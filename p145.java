1d33150f3bd65e95835ade256803b42f18f9691a
Nayuki Minase <nayuki@eigenstate.org>
2012-11-01 04:06:59 +0000

    P145: Sped up Java solution by rewriting the algorithm to do arithmetic in decimal, avoiding division and modulo.

--- p145.java
+++ p145.java
@@ -14,39 +14,48 @@ public final class p145 implements EulerSolution {
 	}
 	
 	
-	private static final int LIMIT = Library.pow(10, 9);
-	
-	
 	public String run() {
 		int count = 0;
-		for (int i = 0; i < LIMIT; i++) {
-			if (isReversible(i))
-				count++;
+		for (int digits = 1; digits <= 9; digits++) {
+			int[] num = new int[digits];  // In base 10, little-endian
+			num[num.length - 1] = 1;      // Initialize to 10^(digits-1)
+			
+			// Iterate until 10^digits - 1
+			do {
+				// Skip if the reverse will have leading zeros
+				if (num[0] != 0 && isReversible(num))
+					count++;
+			} while(increment(num));
 		}
 		return Integer.toString(count);
 	}
 	
 	
-	private static boolean isReversible(int x) {
-		return x % 10 != 0 && hasOnlyOddDigits(x + reverse(x));
-	}
+	private static boolean[] IS_DIGIT_ODD = {false, true, false, true, false, true, false, true, false, true, false, true, false, true, false, true, false, true, false, true};
 	
-	
-	// Assumes x does not have trailing zeros
-	private static int reverse(int x) {
-		int y = 0;
-		for (; x != 0; x /= 10)
-			y = y * 10 + x % 10;
-		return y;
-	}
-	
-	
-	private static boolean hasOnlyOddDigits(int x) {
-		while (x != 0) {
-			if (x % 2 == 0)
+	// Find the sum of num and reverse(num) digit by digit, checking for even digits. Discard the sum.
+	private static boolean isReversible(int[] num) {
+		int carry = 0;
+		for (int i = 0; i < num.length; i++) {
+			int digit = num[i] + num[num.length - 1 - i] + carry;  // In the range 0 to 19 (inclusive)
+			if (!IS_DIGIT_ODD[digit])
 				return false;
-			x /= 10;
+			carry = digit < 10 ? 0 : 1;
 		}
+		// Ignore the final carry-out because it is either 0 (a leading 0 to be ignored) or 1 (which is odd)
+		return true;
+	}
+	
+	
+	private static boolean increment(int[] num) {
+		int i = 0;
+		while (num[i] == 9) {
+			num[i] = 0;
+			i++;
+			if (i == num.length)
+				return false;
+		}
+		num[i]++;
 		return true;
 	}
 	
