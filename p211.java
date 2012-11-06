bf5162275e6dd21a485b3c6d5d5861aec7aa904a
Nayuki Minase <nayuki@eigenstate.org>
2012-11-06 15:40:27 +0000

    P211: Optimized memory usage of Java solution, tweaked code.

--- p211.java
+++ p211.java
@@ -17,7 +17,7 @@ public final class p211 implements EulerSolution {
 	private static final int LIMIT = 64000000;
 	
 	
-	// Requires at least 768 MB of memory
+	// Requires at least 640 MB of memory
 	public String run() {
 		long[] sigma2 = listSigma2(LIMIT - 1);
 		long sum = 0;
@@ -30,15 +30,19 @@ public final class p211 implements EulerSolution {
 	
 	
 	private static long[] listSigma2(int n) {
+		// If i has a prime factor p <= sqrt, then quasiPrimeFactor[i] = p.
+		// Otherwise i > sqrt must be prime, and quasiPrimeFactor[i] = 0 because i may overflow an int16.
+		int sqrt = Library.sqrt(n);
+		short[] quasiPrimeFactor = new short[n + 1];
+		
 		// Richer version of the sieve of Eratosthenes
-		int[] smallestPrimeFactor = new int[n + 1];
-		for (int i = 2; i < smallestPrimeFactor.length; i++) {
-			if (smallestPrimeFactor[i] == 0) {
-				smallestPrimeFactor[i] = i;
+		for (int i = 2; i <= sqrt; i++) {
+			if (quasiPrimeFactor[i] == 0) {
+				quasiPrimeFactor[i] = (short)i;
 				if ((long)i * i <= n) {
 					for (int j = i * i; j <= n; j += i) {
-						if (smallestPrimeFactor[j] == 0)
-							smallestPrimeFactor[j] = i;
+						if (quasiPrimeFactor[j] == 0)
+							quasiPrimeFactor[j] = (short)i;
 					}
 				}
 			}
@@ -47,7 +51,9 @@ public final class p211 implements EulerSolution {
 		long[] sigma2 = new long[n + 1];
 		sigma2[1] = 1;
 		for (int i = 2; i < sigma2.length; i++) {
-			int p = smallestPrimeFactor[i];
+			int p = quasiPrimeFactor[i];
+			if (p == 0)
+				p = i;
 			long sum = 1;
 			int j = i;
 			long p2 = (long)p * p;
@@ -62,8 +68,9 @@ public final class p211 implements EulerSolution {
 	private static boolean isPerfectSquare(long x) {
 		if (x < 0)
 			throw new IllegalArgumentException("Square root of negative number");
+		
 		long y = 0;
-		for (long i = 2147483648L; i != 0; i >>>= 1) {
+		for (long i = 1L << 31; i != 0; i >>>= 1) {
 			y |= i;
 			if (y > 3037000499L || y * y > x)
 				y ^= i;
