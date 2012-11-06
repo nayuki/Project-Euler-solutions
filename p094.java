16145af49df28c6a6e29d6648c37e4e69d5cb820
Nayuki Minase <nayuki@eigenstate.org>
2012-11-06 02:05:35 +0000

    P94: Rewrote Java solution for some speed gain, with explanation.

--- p094.java
+++ p094.java
@@ -14,29 +14,58 @@ public final class p094 implements EulerSolution {
 	}
 	
 	
+	private static final int LIMIT = Library.pow(10, 9);
+	
+	
+	/* 
+	 * Consider an arbitrary almost equilateral triangle with side lengths (c, c, c +/- 1).
+	 * Split it down the middle to get a right triangle, and label the new sides.
+	 *     /\               /|
+	 *  c /  \ c         c / | b
+	 *   /    \    -->    /  |
+	 *  --------         -----
+	 *   c +/- 1           a
+	 * Note that a = (c +/- 1) / 2, and a^2 + b^2 = c^2 (Pythagorean theorem).
+	 * 
+	 * We know that c is an integer. The area of the original triangle is a*b,
+	 * which is an integer by definition from the problem statement.
+	 * - If a is an integer, then b is an integer (so that a*b is an integer),
+	 *   thus (a,b,c) is a Pythagorean triple.
+	 * - Otherwise a is an integer plus a half, then b must be even,
+	 *   but a^2 + b^2 is not an integer, which contradicts c being an integer.
+	 * 
+	 * Conversely, consider an arbitrary Pythagorean triple (a,b,c).
+	 * If 2a = c +/- 1, then we can form an almost equilateral triangle:
+	 *     /|\
+	 *  c / | \ c
+	 *   /  |  \
+	 *  ---------
+	 *      2a
+	 * For this to happen, the Pythagorean triple must be primitive. Because if not,
+	 * then a = 0 mod k and c = 0 mod k for some k > 1, which means 2a = 0 mod k which
+	 * cannot equal c +/- 1 = +/- 1 mod k. So we only need to generate primitive triples.
+	 * 
+	 * Pythagorean triples theorem:
+	 *   Every primitive Pythagorean triple with a odd and b even can be expressed as
+	 *   a = st, b = (s^2-t^2)/2, c = (s^2+t^2)/2, where s > t >= 1 are coprime odd integers.
+	 */
 	public String run() {
 		long sum = 0;
-		
-		// Test triangles with side lengths {i, i, i+1}, excluding {1, 1, 2}
-		for (long i = 2; i * 3 + 1 <= 1000000000; i++) {
-			// The area of the triangle is (i + 1) sqrt(3i^2 - 2i - 1) / 4; check if it's an integer
-			long a = 3*i*i - 2*i - 1;
-			long b = Library.sqrt(a);
-			if (b * b != a || b * (i + 1) % 4 != 0)
-				continue;
-			sum += i * 3 + 1;
+		for (int s = 1; s * s <= LIMIT; s++) {
+			for (int t = 1; t < s; t++) {
+				if (Library.gcd(s, t) == 1) {
+					int a = s * t;
+					int b = (s * s - t * t) / 2;
+					int c = (s * s + t * t) / 2;
+					if (a * 2 == c - 1) { int p = c * 3 - 1; if (p <= LIMIT) sum += p; }
+					if (a * 2 == c + 1) { int p = c * 3 + 1; if (p <= LIMIT) sum += p; }
+					// Swap the roles of a and b and try the same tests
+					// Note that a != b, since otherwise c = a * sqrt(2) would be irrational
+					if (b * 2 == c - 1) { int p = c * 3 - 1; if (p <= LIMIT) sum += p; }
+					if (b * 2 == c + 1) { int p = c * 3 + 1; if (p <= LIMIT) sum += p; }
+				}
+			}
 		}
-		
-		// Test triangles with side lengths {i+1, i+1, i}
-		for (long i = 1; i * 3 + 2 <= 1000000000; i++) {
-			// The area of the triangle is i sqrt(3i^2 + 8i + 4) / 4; check if it's an integer
-			long a = 3*i*i + 8*i + 4;
-			long b = Library.sqrt(a);
-			if (b * b != a || b * i % 4 != 0)
-				continue;
-			sum += i * 3 + 2;
-		}
-		
 		return Long.toString(sum);
 	}
 	
