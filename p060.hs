3538f55b1cf6c20ac076956e883b402e039ed6d1
Nayuki Minase <nayuki@eigenstate.org>
2013-04-30 13:21:24 +0000

    P60: Added Haskell solution.

---
+++ p060.hs
@@ -0,0 +1,40 @@
+{- 
+ - Solution to Project Euler problem 60
+ - By Nayuki Minase
+ - 
+ - http://nayuki.eigenstate.org/page/project-euler-solutions
+ - https://github.com/nayuki/Project-Euler-solutions
+ -}
+
+import Data.Numbers.Primes (isPrime, primes)  -- From http://hackage.haskell.org/package/primes
+
+
+-- Arbitrary initial limit for the sum of the set of chosen concatenatable primes.
+-- If sufficiently large then the answer will be correct; otherwise it will be -1.
+initialLimit = 100000
+
+setSize = 5
+
+
+findSmallerSetSum :: Int -> Int -> Int
+findSmallerSetSum limit bestAns = let nextAns = (findSetSum [] limit primes) in
+	if nextAns /= -1 then (findSmallerSetSum (nextAns - 1) nextAns) else bestAns
+
+findSetSum :: [Int] -> Int -> [Int] -> Int
+findSetSum foundSet limit nextPrimes
+	| length foundSet == setSize = sum foundSet
+	| otherwise = findCandidate nextPrimes
+	where
+		findCandidate (p:nextPrimes)
+			| p > limit = -1
+			| ((all (isConcatPrime p) foundSet) && s /= -1) = s
+			| otherwise = findCandidate nextPrimes
+			where
+				s = findSetSum (p:foundSet) (limit - p) nextPrimes
+
+isConcatPrime :: Int -> Int -> Bool
+isConcatPrime x y = (isPrime $ readInt $ show x ++ show y) && (isPrime $ readInt $ show y ++ show x)
+	where readInt = read :: String -> Int
+
+ans = findSmallerSetSum initialLimit (-1)
+main = putStrLn (show ans)
