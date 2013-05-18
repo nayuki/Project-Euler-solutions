cc7d94bfeff3826197263ac8e77d53ae59cc29c8
Nayuki Minase <nayuki@eigenstate.org>
2013-05-18 16:45:09 +0000

    P301: Optimized solutions using math.

--- p301.java
+++ p301.java
@@ -14,10 +14,27 @@ public final class p301 implements EulerSolution {
 	}
 	
 	
+	/* 
+	 * In a game of Nim where both players play with perfect strategy, if the current state is a collection (multiset) of piles
+	 * with sizes {n1, n2, ..., n_m}, then the current player will lose if and only if n1 XOR n2 XOR ... XOR n_m = 0.
+	 * In this problem, we specialize the condition to just n XOR 2n XOR 3n = 0.
+	 * 
+	 * Facts:
+	 *   3n = 2n + n.
+	 *   a ^ b = 0 iff a = b. (from digital logic)
+	 *   a + b = (a ^ b) + ((a & b) << 1). (from digital logic)
+	 * Hence:
+	 *   n ^ 2n ^ 3n = 0                          (what we want)
+	 *   iff n ^ 2n = 3n                          (from the second fact)
+	 *   iff n ^ 2n = (n ^ 2n) + ((n & 2n) << 1)  (from the third fact)
+	 *   iff (n & 2n) << 1 = 0                    (by cancelling on both sides)
+	 *   iff n & 2n = 0                           (left-shifting doesn't change zeroness)
+	 *   iff the binary representation of n does not have consecutive '1' bits.
+	 */
 	public String run() {
 		int count = 0;
-		for (int i = 1; i <= (1 << 30); i++) {
-			if ((i ^ (i * 2) ^ (i * 3)) == 0)
+		for (int n = 1; n <= (1 << 30); n++) {
+			if ((n & (n << 1)) == 0)
 				count++;
 		}
 		return Integer.toString(count);
