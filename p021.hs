6126999afde95221cdaaf5e0cf9450671a3d1763
Nayuki Minase <nayuki@eigenstate.org>
2013-07-24 02:33:38 +0000

    P21: Added Haskell solution.

---
+++ p021.hs
@@ -0,0 +1,14 @@
+{- 
+ - Solution to Project Euler problem 21
+ - By Nayuki Minase
+ - 
+ - http://nayuki.eigenstate.org/page/project-euler-solutions
+ - https://github.com/nayuki/Project-Euler-solutions
+ -}
+
+
+main = putStrLn (show ans)
+ans = sum [n | n <- [1..10^4], amicable n]
+
+amicable n = let m = divisorSum n in (m /= n) && (divisorSum m) == n
+divisorSum n = sum [k | k <- [1..n-1], (mod n k) == 0]
