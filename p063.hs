4eb7f1211fdb434ec63690a585b6a66ed9810fdf
Nayuki Minase <nayuki@eigenstate.org>
2014-06-12 04:34:54 +0000

    P63: Added and rewrote explanations to all 3 solutions, tweaked code.

--- p063.hs
+++ p063.hs
@@ -7,8 +7,24 @@
  -}
 
 
+{- 
+ - Let's examine n^k for different values of n and k and see which
+ - choices cannot possibly work (i.e. not being exactly k digits long).
+ - 
+ - When n = 10, for each k, n^k has exactly k+1 digits, so these are excluded.
+ - By extension, when n > 10, for each k, n^k has at least k+1 digits, so these are excluded.
+ - Thus we should only consider 1 <= n <= 9.
+ - 
+ - When n = 9, k = 22, then n^k has 21 digits which is insufficient.
+ - Extending this, when n = 9 and k > 22, n^k has fewer than k digits.
+ - Furthermore, when n < 9, n^k will have start to have
+ - fewer than k digits at some value of k with k < 22.
+ - Therefore we should only consider 1 <= k <= 21.
+ - 
+ - We handle the rest of the testing by brute force.
+ -}
 main = putStrLn (show ans)
-ans = sum [1 | n <- [1..21], i <- [1..9], (numLength (i^n :: Integer)) == n]
+ans = sum [1 | n <- [1..9], k <- [1..21], (numLength (n^k :: Integer)) == k]
 
 numLength :: Integer -> Int
 numLength n | n < 10    = 1
--- p063.java
+++ p063.java
@@ -16,11 +16,27 @@ public final class p063 implements EulerSolution {
 	}
 	
 	
+	/* 
+	 * Let's examine n^k for different values of n and k and see which
+	 * choices cannot possibly work (i.e. not being exactly k digits long).
+	 * 
+	 * When n = 10, for each k, n^k has exactly k+1 digits, so these are excluded.
+	 * By extension, when n > 10, for each k, n^k has at least k+1 digits, so these are excluded.
+	 * Thus we should only consider 1 <= n <= 9.
+	 * 
+	 * When n = 9, k = 22, then n^k has 21 digits which is insufficient.
+	 * Extending this, when n = 9 and k > 22, n^k has fewer than k digits.
+	 * Furthermore, when n < 9, n^k will have start to have
+	 * fewer than k digits at some value of k with k < 22.
+	 * Therefore we should only consider 1 <= k <= 21.
+	 * 
+	 * We handle the rest of the testing by brute force.
+	 */
 	public String run() {
 		int count = 0;
-		for (int n = 1; n <= 21; n++) {
-			for (int i = 1; i <= 9; i++) {
-				if (BigInteger.valueOf(i).pow(n).toString().length() == n)
+		for (int n = 1; n <= 9; n++) {
+			for (int k = 1; k <= 21; k++) {
+				if (BigInteger.valueOf(n).pow(k).toString().length() == k)
 					count++;
 			}
 		}
