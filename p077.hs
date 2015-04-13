f627cd554a859f243ee1f6c798f080629745ccf8
Nayuki Minase <nayuki@eigenstate.org>
2015-04-13 17:54:51 +0000

    P77: Added Haskell and Mathematica solutions, updated Java solution.

---
+++ p077.hs
@@ -0,0 +1,32 @@
+{- 
+ - Solution to Project Euler problem 77
+ - by Project Nayuki
+ - 
+ - http://www.nayuki.io/page/project-euler-solutions
+ - https://github.com/nayuki/Project-Euler-solutions
+ -}
+
+
+target = 5000
+main = putStrLn (show ans)
+ans = head $ dropWhile (\n -> partitions n n <= target) [0..]
+
+{- 
+ - Let P(i, n) denote the number of ways that n can be written as an
+ - unordered sum of prime numbers where no prime is greater than i.
+ - 
+ - * P(i, 0) = 1 for all i.
+ - * P(0, n) = 0 for all n > 0.
+ - * If i is 1 or composite then P(i, n) = P(i - 1, n).
+ - * Otherwise i is prime:
+ -   * If i <= n then P(i, n) = P(i - 1, n) + P(i, n - i).
+ -   * Else P(i, n) = P(i - 1, n).
+ -}
+partitions :: Int -> Int -> Int
+partitions _ 0 = 1
+partitions 0 _ = 0
+partitions i n = (if ((isPrime i) && i <= n) then (partitionsMemo !! i !! (n - i)) else 0) + (partitionsMemo !! (i - 1) !! n)
+partitionsMemo = [[partitions i n | n <- [0..]] | i <- [0..]]
+
+isPrime :: Int -> Bool
+isPrime n = n > 1 && null [() | k <- [2 .. n-1], mod n k == 0]
--- p077.java
+++ p077.java
@@ -18,30 +18,43 @@ public final class p077 implements EulerSolution {
 	
 	public String run() {
 		for (int limit = 1; ; limit *= 2) {
-			int result = search(limit);
+			int result = search(limit, TARGET);
 			if (result != -1)
 				return Integer.toString(result);
 		}
 	}
 	
 	
-	private static int search(int limit) {
-		int[] primes = Library.listPrimes(limit);
-		// partitions[i][j] is the number of ways (with upper saturation at TARGET)
-		// that j can be written as an unordered sum with terms drawn from the first i prime numbers
-		int[][] partitions = new int[primes.length + 1][limit + 1];
-		partitions[0][0] = 1;
-		for (int i = 0; i < primes.length; i++) {
-			int p = primes[i];
-			for (int j = 0; j <= limit; j++) {
-				int sum = partitions[i][j];
-				if (j >= p)
-					sum += partitions[i + 1][j - p];
-				partitions[i + 1][j] = Math.min(sum, TARGET);  // Saturate to prevent overflow
-			}
+	/* 
+	 * This function searches for the smallest n in [0, limit)
+	 * such that P(n, n) > target, or returns -1 if not found.
+	 * 
+	 * Let P(i, n) denote the number of ways that n can be written as an
+	 * unordered sum of prime numbers where no prime is greater than i.
+	 * 
+	 * - P(i, 0) = 1 for all i.
+	 * - P(0, n) = 0 for all n > 0.
+	 * - If i is 1 or composite then P(i, n) = P(i - 1, n).
+	 * - Otherwise i is prime:
+	 *   - If i <= n then P(i, n) = P(i - 1, n) + P(i, n - i).
+	 *   - Else P(i, n) = P(i - 1, n).
+	 * 
+	 * Notice that when computing P(i, k), we only need values from the
+	 * current row i for k' in [1, k) and values from the previous row i -1
+	 * for k' in [k, n). Thus we only need to buffer one row of data for
+	 * dynamic programming and can overwrite it in place.
+	 */
+	private static int search(int limit, int target) {
+		int[] partitions = new int[limit];
+		partitions[0] = 1;
+		for (int i = 0; i < partitions.length; i++) {
+			if (!Library.isPrime(i))
+				continue;
+			for (int j = i; j < partitions.length; j++)
+				partitions[j] += partitions[j - i];
 		}
-		for (int i = 0; i <= limit; i++) {
-			if (partitions[primes.length][i] >= TARGET)
+		for (int i = 0; i < limit; i++) {
+			if (partitions[i] > target)
 				return i;
 		}
 		return -1;
