47dcfd922c56e8b15ecb93985a6212a88395b5c8
Nayuki Minase <nayuki@eigenstate.org>
2015-04-13 18:16:25 +0000

    P118: Simplified and optimized Java solution.

--- p118.java
+++ p118.java
@@ -6,8 +6,6 @@
  * https://github.com/nayuki/Project-Euler-solutions
  */
 
-import java.util.Arrays;
-
 
 public final class p118 implements EulerSolution {
 	
@@ -16,59 +14,38 @@ public final class p118 implements EulerSolution {
 	}
 	
 	
-	private int[] isPrime;
-	
-	
 	public String run() {
-		listPrimality(Library.pow(10, 9) - 1);
-		
-		// Try all permutations, and try all splits within each permutation
+		isPrime = Library.listPrimality(10000);
+		count = 0;
 		int[] digits = {1, 2, 3, 4, 5, 6, 7, 8, 9};
-		countBySize = new int[10];
-		do countPrimeSets(digits, 0, 0);
+		do countPrimeSets(digits, 0, 0);  // Try all permutations, and try all splits within each permutation
 		while (Library.nextPermutation(digits));
-		
-		// Eliminate permutation duplicates
-		int count = 0;
-		for (int i = 0; i < countBySize.length; i++)
-			count += countBySize[i] / FACTORIAL[i];
 		return Integer.toString(count);
 	}
 	
 	
-	private int[] countBySize;
+	private boolean[] isPrime;  // Cache for small numbers
+	private int count;
 	
 	
-	private void countPrimeSets(int[] digits, int start, int size) {
-		if (start == digits.length)
-			countBySize[size]++;
+	private void countPrimeSets(int[] digits, int startIndex, int prevNum) {
+		if (startIndex == digits.length)
+			count++;
 		else {
-			for (int split = start + 1; split <= digits.length; split++) {
-				if (isPrime(toInteger(digits, start, split)))
-					countPrimeSets(digits, split, size + 1);
+			for (int split = startIndex + 1; split <= digits.length; split++) {
+				int num = toInteger(digits, startIndex, split);
+				if (num > prevNum && isPrime(num))
+					countPrimeSets(digits, split, num);
 			}
 		}
 	}
 	
 	
-	// Do prime sieve but store bitwise (instead of the usual Boolean array)
-	private void listPrimality(int n) {
-		isPrime = new int[n / 32 + 1];
-		Arrays.fill(isPrime, ~0);
-		isPrime[0] &= ~3;  // 0 and 1 are not prime
-		
-		// Sieve of Eratosthenes
-		for (int i = 2, end = Library.sqrt(n); i <= end; i++) {
-			if (isPrime(i)) {
-				for (int j = i * i; j <= n; j += i)
-					isPrime[j >>> 5] &= ~(1 << (j & 0x1F));
-			}
-		}
-	}
-	
-	
-	private boolean isPrime(int x) {
-		return (isPrime[x >>> 5] >>> (x & 0x1F) & 1) != 0;
+	private boolean isPrime(int n) {
+		if (n < isPrime.length)
+			return isPrime[n];
+		else
+			return Library.isPrime(n);
 	}
 	
 	
@@ -78,9 +55,5 @@ public final class p118 implements EulerSolution {
 			result = result * 10 + digits[i];
 		return result;
 	}
-	
-	
-	// Hard-coded values for factorial(0), factorial(1), ..., factorial(9)
-	private static int[] FACTORIAL = {1, 1, 2, 6, 24, 120, 720, 5040, 40320, 362880};
 	
 }
