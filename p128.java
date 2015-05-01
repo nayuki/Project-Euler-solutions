32787d716dda021172ac090b3acc57afcaf8a6d4
Nayuki Minase <nayuki@eigenstate.org>
2015-05-01 06:11:55 +0000

    P128: Completely rewrote Java solution with mathematical analysis and much simpler and faster code.

--- p128.java
+++ p128.java
@@ -14,254 +14,172 @@ public final class p128 implements EulerSolution {
 	}
 	
 	
-	public String run() {
-		return Long.toString(findSolution(2000));
-	}
-	
-	
-	private static long findSolution(int targetIndex) {  // 1-based counting
-		int count = 1;  // Because PD(1) = 3
-		boolean printed = false;
-		for (int layer = 1; ; layer++) {
-			for (int sextant = 0; sextant < 6; sextant++) {
-				for (int offset = 0; offset < layer; offset++) {
-					long i = lsoToNumber(packLso(layer, sextant, offset));
-					if (!printed) {  // Progress reporting for the impatient
-						System.out.println(count + " " + i);
-						printed = true;
-					}
-					if (countPrimeNeighbors(i) == 3) {
-						count++;
-						printed = false;
-						if (count == targetIndex)
-							return i;
-					}
-					if (offset == 1 && layer > 2)  // Skip to penultimate offset
-						offset = layer - 2;
-				}
-			}
-		}
-	}
-	
-	
-	/* 
-	 * Coordinate system of (u, v):
-	 *         +u
-	 *   +u+v  |   -v
-	 *       \ | /
-	 *         *
-	 *       / | \
-	 *    +v   |  -u-v
-	 *        -u
+	/*
+	 * Let's do some mathematical analysis to drastically reduce the amount
+	 * of logic we need to implement and calculation the computer needs to do.
+	 * We begin with a couple of definitions.
 	 * 
-	 * Actual coordinates:
-	 *             (+2,+0)
-	 *        (+2,+1)   (+1,-1)
-	 *   (+2,+2)   (+1,+0)   (+0,-2)
-	 *        (+1,+1)   (+0,-1)
-	 *   (+1,+2)   (+0,+0)   (-1,-2)
-	 *        (+0,+1)   (-1,-1)
-	 *   (+0,+2)   (-1,+0)   (-2,-2)
-	 *        (-1,+1)   (-2,-1)
-	 *             (-2,+0)
+	 * Each cell belongs in a numbered hexagonal ring. The rings
+	 * are numbered starting from 0 at the center like this:
+	 *               3
+	 *           3       3
+	 *       3       2       3
+	 *   3       2       2       3
+	 *       2       1       2
+	 *   3       1       1       3
+	 *       2       0       2
+	 *   3       1       1       3
+	 *       2       1       2
+	 *   3       2       2       3
+	 *       3       2       3
+	 *           3       3
+	 *               3
 	 * 
-	 * Layer system:
-	 *            3
-	 *         3     3
-	 *      3     2     3
-	 *   3     2     2     3
-	 *      2     1     2
-	 *   3     1     1     3
-	 *      2     0     2
-	 *   3     1     1     3
-	 *      2     1     2
-	 *   3     2     2     3
-	 *      3     2     3
-	 *         3     3
-	 *            3
+	 * Within a ring, each cell is either a corner cell or an edge cell, as shown:
+	 *               C
+	 *           E       E
+	 *       E       C       E
+	 *   C       E       E       C
+	 *       C       C       C
+	 *   E       C       C       E
+	 *       E       C       E
+	 *   E       C       C       E
+	 *       C       C       C
+	 *   C       E       E       C
+	 *       E       C       E
+	 *           E       E
+	 *               C
 	 * 
-	 * Sextant system:
-	 *           0
-	 *        0     5
-	 *     0     0     5
-	 *  1     0     5     5
-	 *     1     0     5
-	 *  1     1     5     4
-	 *     1     0     4
-	 *  1     2     4     4
-	 *     2     3     4
-	 *  2     2     3     4
-	 *     2     3     3
-	 *        2     3
-	 *           3
+	 * Basic observations:
+	 * - Ring k has 6k cells (except the degenerate ring 0 which has 1 cell).
+	 * - In the code we will skip the PD (prime difference) calculation for
+	 *   rings 0 and 1 because the existence of ring 0 breaks many patterns.
+	 * - Doing the PD calculation for rings 0 and 1 (cells 1 to 7 inclusive)
+	 *   by hand, we find that PD(n) = 3 for and only for n = 1, 2.
 	 * 
-	 * Offset system:
-	 *            0
-	 *         1     2
-	 *      2     0     1
-	 *   0     1     1     0
-	 *      0     0     0
-	 *   1     0     0     2
-	 *      1     0     1
-	 *   2     0     0     1
-	 *      0     0     0
-	 *   0     1     1     0
-	 *      1     0     2
-	 *         2     1
-	 *            0
+	 * Now let's analyze the characteristics of all cells in rings 2 or above.
+	 * It's hard to justify these assertions rigorously, but they are true from
+	 * looking at the spiral diagram.
 	 * 
-	 * Note: LSO stands for layer, sextant, offset.
+	 * - Corner cells along the upward vertical direction and the edge cells immediately to the right
+	 *   of this vertical column are the most interesting, so we will save these cases for last.
+	 * 
+	 * - Claim: Except for ones immediately right of the upward corner column, no edge cell satisfies PD(n) = 3.
+	 *   Proof: Take an arbitrary edge cell n not immediately to the right of the upward corner column...
+	 *   - The two neighbors in the same ring have a difference of 1 compared to n,
+	 *     which is not a prime number.
+	 *   - The two neighbors in the previous (inward) ring are consecutive numbers,
+	 *     so exactly one of them has an even absolute difference with n. Because n
+	 *     is in ring 2 or above, the difference with any number in the previous ring
+	 *     is at least 6. Thus an even number greater than 2 is not prime.
+	 *   - Similarly, the two neighbors in the next (outward) ring are consecutive numbers.
+	 *     One of them has an even difference with n, and this number is also at least 6,
+	 *     so one neighbor is definitely not prime.
+	 *   - Therefore with at least 4 neighbors that do not have a prime difference, PD(n) <= 2.
+	 *   Example of edge cell n = 11 in ring 2, which is straight left of the origin:
+	 *         10
+	 *     24      03
+	 *         11
+	 *     25      04
+	 *         12
+	 * 
+	 * - Claim: No corner cell in the other 5 directions satisfies PD(n) = 3.
+	 *   Proof: Take an arbitrary corner cell n in the non-upward direction...
+	 *   - Two of its neighbors (in the same ring) have a difference of 1, which is not prime.
+	 *   - One neighbor is in the previous ring (inward) while three neighbors are in the next ring (outward).
+	 *   - Let the inner ring neighbor be k and the outer ring's middle neighbor be m. Then n - k + 6 = m - n.
+	 *   - The three outer ring neighbors are {m - 1, m, m + 1}.
+	 *   - Either both {|k - n|, |m - n|} are even or both {|m - 1 - n|, |m + 1 - n|} are even.
+	 *     In any case, all these differences are at least 6, so the even numbers are not prime.
+	 *   - Therefore with at least 4 neighbors that do not have a prime difference, PD(n) <= 2.
+	 *   Example of edge cell n = 14 in ring 2, which is straight below the origin:
+	 *         05
+	 *     13      15
+	 *         14
+	 *     28      30
+	 *         29
+	 * 
+	 * - Now let's consider an arbitrary upward corner cell n in ring k, with k >= 2.
+	 *   We shall give variables to all its neighbors like this:
+	 *         d
+	 *     e       f
+	 *         n
+	 *     b       c
+	 *         a
+	 *   - a is in the previous ring, {b, c} are in the same ring as n, and {d, e, f} are in the next ring.
+	 *   - Equations derived from the structure of the hexagonal spiral:
+	 *     n = 3k(k - 1) + 2.
+	 *     a = n - 6(k - 1).
+	 *     b = n + 1.
+	 *     c = n + 6k - 1 = d - 1.
+	 *     d = n + 6k.
+	 *     e = n + 6k + 1 = d + 1.
+	 *     f = n + 6k + 6(k + 1) - 1 = n + 12k + 5.
+	 *   - Hence we get these absolute differences with n:
+	 *     |a - n| = 6(k - 1). (Not prime because it's a multiple of 6 and at least 6)
+	 *     |b - n| = 1. (Not prime)
+	 *     |c - n| = 6k - 1. (Possibly prime)
+	 *     |d - n| = 6k. (Not prime because it's a multiple of 6 and at least 12)
+	 *     |e - n| = 6k + 1. (Possibly prime)
+	 *     |f - n| = 12k + 5. (Possibly prime)
+	 *   - Therefore for each k >= 2, we count how many numbers in the set {6k - 1, 6k + 1, 12k + 5} are prime.
+	 *   Example of corner cell n = 8 in ring 2, which is straight above the origin:
+	 *         20
+	 *     21      37
+	 *         08
+	 *     09      19
+	 *         02
+	 * 
+	 * - Finally let's consider an arbitrary edge cell immediately to the right of the upward vertical column.
+	 *   Suppose the cell's value is n and it is in ring k, with k >= 2. Give variables to all its neighbors like this:
+	 *         f
+	 *     c       e
+	 *         n
+	 *     a       d
+	 *         b
+	 *   - {a, b} are in the previous ring, {c, d} are in the current ring, and {e, f} are in the next ring.
+	 *     The ascending ordering of all these numbers is (a, b, c, d, n, e, f).
+	 *   - Equations derived from the structure of the hexagonal spiral:
+	 *     n = 3k(k + 1) + 1.
+	 *     a = n - 6k - 6(k - 1) + 1 = n - 12k + 7.
+	 *     b = n - 6k.
+	 *     c = n - 6k + 1.
+	 *     d = n - 1.
+	 *     e = n + 6(k + 1) - 1 = n + 6k + 5.
+	 *     f = n + 6(k + 1).
+	 *   - Hence we get these absolute differences with n:
+	 *     |a - n| = 12k - 7. (Possibly prime)
+	 *     |b - n| = 6k. (Not prime because it's a multiple of 6 and at least 12)
+	 *     |c - n| = 6k - 1. (Possibly prime)
+	 *     |d - n| = 1. (Not prime)
+	 *     |e - n| = 6k + 5. (Possibly prime)
+	 *     |f - n| = 6(k + 1). (Not prime because it's a multiple of 6 and at least 18)
+	 *   Example of edge cell n = 19 in ring 2:
+	 *         37
+	 *     08      36
+	 *         19
+	 *     02      18
+	 *         07
 	 */
 	
-	private static long numberToLso(long n) {
-		if (n <= 0)
-			throw new IllegalArgumentException();
-		
-		int layer, sextant, offset;
-		if (n == 1) {
-			layer = 0;
-			sextant = 0;
-			offset = 0;
-		} else {
-			n -= 2;
-			layer = 1;
-			for (int layerSize = layer * 6; n >= layerSize; layerSize += 6) {
-				if (layer > Integer.MAX_VALUE / 6)
-					throw new ArithmeticException();
-				n -= layerSize;
-				layer++;
-			}
-			offset = (int)(n % layer);
-			sextant = (int)(n / layer);
-		}
-		return packLso(layer, sextant, offset);
-	}
+	private static final int TARGET = 2000;  // Must be at least 3
 	
-	
-	private static long lsoToCoordinate(long lso) {
-		int u, v;
-		if (lso == 0) {
-			u = 0;
-			v = 0;
-			
-		} else {
-			int layer = (int)(lso >>> 33);
-			int sextant = (int)(lso >>> 30) & 7;
-			int offset = (int)lso & ((1 << 30) - 1);
-			if (layer < 0 || sextant >= 6 || offset < 0 || offset >= layer)
-				throw new IllegalArgumentException();
-			
-			switch (sextant) {
-				case 0:  u = layer;           v = offset;          break;
-				case 1:  u = layer - offset;  v = layer;           break;
-				case 2:  u = -offset;         v = layer - offset;  break;
-				case 3:  u = -layer;          v = -offset;         break;
-				case 4:  u = offset - layer;  v = -layer;          break;
-				case 5:  u = offset;          v = offset - layer;  break;
-				default:  throw new AssertionError();
-			}
-		}
-		return packCoordinate(u, v);
-	}
-	
-	
-	private static long coordinateToLso(long uv) {
-		int u = (int)(uv >>> 32), v = (int)uv;
-		if (u == 0 && v == 0)
-			return 0;
-		
-		int sextant, layer, offset;
-		if (u > 0 && v >= 0 && v < u) {
-			sextant = 0;
-			layer = u;
-			offset = v;
-		} else if (v > 0 && u > 0 && u <= v) {
-			sextant = 1;
-			layer = v;
-			offset = layer - u;
-		} else if (u <= 0 && v > 0) {
-			sextant = 2;
-			layer = v - u;
-			offset = -u;
-		} else if (u < 0 && v <= 0 && v > u) {
-			sextant = 3;
-			layer = -u;
-			offset = -v;
-		} else if (v < 0 && u < 0 && u >= v) {
-			sextant = 4;
-			layer = -v;
-			offset = u - v;
-		} else if (u >= 0 && v < 0) {
-			sextant = 5;
-			layer = u - v;
-			offset = u;
-		} else
-			throw new AssertionError();
-		return packLso(layer, sextant, offset);
-	}
-	
-	
-	private static long lsoToNumber(long lso) {
-		if (lso == 0)
-			return 1;
-		
-		int layer = (int)(lso >>> 33);
-		int sextant = (int)(lso >>> 30) & 7;
-		int offset = (int)lso & ((1 << 30) - 1);
-		if (layer < 0 || sextant >= 6 || offset < 0 || offset >= layer)
-			throw new IllegalArgumentException();
-		
-		long n = offset + sextant * layer + 2;
-		for (int i = 1; i < layer; i++)
-			n += i * 6;
-		return n;
-	}
-	
-	
-	private static long packLso(int layer, int sextant, int offset) {
-		return (long)layer << 33 | (long)sextant << 30 | offset;  // All parts are unsigned
-	}
-	
-	
-	private static long packCoordinate(int u, int v) {
-		return (long)u << 32 | (v & 0xFFFFFFFFL);  // Both parts are signed int32
-	}
-	
-	
-	/* Other functions */
-	
-	private static int countPrimeNeighbors(long n) {
-		long uv = lsoToCoordinate(numberToLso(n));
-		int u = (int)(uv >>> 32), v = (int)uv;
-		int count = 0;
-		for (int[] delta : HEX_NEIGHBOR_DELTAS) {
-			int x = u + delta[0];
-			int y = v + delta[1];
-			long neigh = lsoToNumber(coordinateToLso(packCoordinate(x, y)));
-			if (isPrime(Math.abs(neigh - n)))
+	public String run() {
+		int count = 2;  // Because n = 1 and 2 satisfy PD(n) = 3
+		for (int ring = 2; ; ring++) {
+			if ((long)ring * 12 + 5 > Integer.MAX_VALUE)
+				throw new ArithmeticException();
+			if (Library.isPrime(ring * 6 - 1) && Library.isPrime(ring * 6 + 1) && Library.isPrime(ring * 12 + 5)) {  // Corner test
 				count++;
-		}
-		return count;
-	}
-	
-	
-	private static boolean isPrime(long x) {
-		if (x < 0)
-			throw new IllegalArgumentException("Negative number");
-		if (x < 6)
-			return x == 2 || x == 3 || x == 5;
-		else if (x % 2 == 0 || x % 3 == 0 || x % 5 == 0)
-			return false;
-		else {
-			for (long i = 7, end = Library.sqrt(x); i <= end; i += 6) {
-				if (x % i == 0 || x % (i + 4) == 0)  // Wheel factorization
-					return false;
+				if (count == TARGET)
+					return Long.toString((long)ring * (ring - 1) * 3 + 2);
 			}
-			return true;
+			if (Library.isPrime(ring * 6 - 1) && Library.isPrime(ring * 6 + 5) && Library.isPrime(ring * 12 - 7)) {  // Edge test
+				count++;
+				if (count == TARGET)
+					return Long.toString((long)ring * (ring + 1) * 3 + 1);
+			}
 		}
 	}
-	
-	
-	private static final int[][] HEX_NEIGHBOR_DELTAS = {{-1,0}, {+1,0}, {0,-1}, {0,+1}, {-1,-1}, {+1,+1}};
 	
 }
