aa82c55e58e52b7f3b5c4f43727664eb994e0005
Nayuki Minase <nayuki@eigenstate.org>
2015-07-23 01:29:18 +0000

    P46: De-optimized Java code negligibly to improve simplicity.

--- p046.java
+++ p046.java
@@ -15,32 +15,23 @@ public final class p046 implements EulerSolution {
 	
 	
 	public String run() {
-		for (int i = 2; ; i++) {
+		for (int i = 9; ; i += 2) {
 			if (!satisfiesConjecture(i))
 				return Integer.toString(i);
 		}
 	}
 	
 	
-	private boolean satisfiesConjecture(int n) {
-		if (n % 2 == 0 || isPrime(n))
+	private static boolean satisfiesConjecture(int n) {
+		if (n % 2 == 0 || Library.isPrime(n))
 			return true;
 		
 		// Now n is an odd composite number
 		for (int i = 1; i * i * 2 <= n; i++) {
-			if (isPrime(n - i * i * 2))
+			if (Library.isPrime(n - i * i * 2))
 				return true;
 		}
 		return false;
-	}
-	
-	
-	private boolean[] isPrime = {};
-	
-	private boolean isPrime(int n) {
-		if (n >= isPrime.length)
-			isPrime = Library.listPrimality(n * 2);
-		return isPrime[n];
 	}
 	
 }
