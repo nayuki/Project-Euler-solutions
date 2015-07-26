4df234cd0db406a2c101bc5925d8a23cdba7e3ef
rindazzz <rindazzz@gmail.com>
2015-07-27 01:35:42 +0300

    Update p004.py

--- python/p004.py
+++ python/p004.py
@@ -9,12 +9,14 @@
 
 def compute():
 	ans = 0
-	for i in range(100, 1000):
-		for j in range(100, 1000):
+	stop = 100
+	for i in range(1000, 100, -1):
+		for j in range(1000, stop, -1):
 			k = i * j
 			s = str(k)
 			if s == s[::-1] and k > ans:
 				ans = k
+				stop = j
 	return str(ans)
 
 
