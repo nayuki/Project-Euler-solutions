4dd2e103422b7813f0e1b97845b967e36018890a
Nayuki Minase <nayuki@eigenstate.org>
2015-08-25 20:28:35 +0000

    P249: Added Python solution.

---
+++ python/p249.py
@@ -0,0 +1,31 @@
+# 
+# Solution to Project Euler problem 249
+# by Project Nayuki
+# 
+# http://www.nayuki.io/page/project-euler-solutions
+# https://github.com/nayuki/Project-Euler-solutions
+# 
+
+import eulerlib
+
+
+def compute():
+	LIMIT = 5000
+	MODULUS = 10**16
+	
+	# Use dynamic programming. count[i] is the number of subsets of primes with the sum of i, modulo MODULUS.
+	count = [0] * (LIMIT**2 // 2)
+	count[0] = 1
+	s = 0  # Sum of all primes seen so far, and thus the highest index among nonzero entries in 'count'
+	for p in eulerlib.list_primes(LIMIT):
+		for i in range(s, -1, -1):
+			count[i + p] = (count[i + p] + count[i]) % MODULUS
+		s += p
+	
+	isprime = eulerlib.list_primality(len(count))
+	ans = sum(count[i] for i in range(len(count)) if isprime[i]) % MODULUS
+	return str(ans)
+
+
+if __name__ == "__main__":
+	print(compute())
