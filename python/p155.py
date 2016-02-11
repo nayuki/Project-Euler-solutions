b1ec01d6e140f4b1166ed3902f492035f829efe7
Nayuki Minase <nayuki@eigenstate.org>
2016-02-11 21:02:21 +0000

    P155: Significantly sped up Python solution at the expense of clarity.

--- python/p155.py
+++ python/p155.py
@@ -16,17 +16,23 @@ def compute():
 	# capacitor network that uses exactly i capacitors of 60 uF each
 	possible = []
 	all = set()  # Union of every possible[i]
+	# Note: Each fraction is represented as a pair (num, den), where den > 0 and gcd(num, den) = 1.
+	# This approach is much faster than using the fractions.Fraction class.
 	possible.append(set())
-	possible.append(set([fractions.Fraction(60, 1)]))
+	possible.append(set([(60, 1)]))
 	all.update(possible[1])
 	for i in range(2, SIZE + 1):
 		poss = set()
 		for j in range(1, i // 2 + 1):
-			for a in possible[j]:
-				for b in possible[i - j]:
-					sum = a + b
-					poss.add(sum)  # Parallel
-					poss.add(a * b / sum)  # Series
+			for (n0, d0) in possible[j]:
+				for (n1, d1) in possible[i - j]:
+					pseudosum = n0 * d1 + n1 * d0
+					numerprod = n0 * n1
+					denomprod = d0 * d1
+					npgcd = fractions.gcd(pseudosum, numerprod)
+					dpgcd = fractions.gcd(pseudosum, denomprod)
+					poss.add((pseudosum // dpgcd, denomprod // dpgcd))  # Parallel
+					poss.add((numerprod // npgcd, pseudosum // npgcd))  # Series
 		possible.append(poss)
 		all.update(poss)
 	return str(len(all))
