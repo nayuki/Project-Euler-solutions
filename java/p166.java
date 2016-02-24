92f17e283c38e29d32ddd9e71bda9fd55356de5d
Nayuki Minase <nayuki@eigenstate.org>
2016-02-24 20:31:09 +0000

    P166: Revamped solutions - added extensive mathematical explanation, sped up Python code significantly, simplified Java code.

--- java/p166.java
+++ java/p166.java
@@ -16,49 +16,65 @@ public final class p166 implements EulerSolution {
 	
 	/* 
 	 * Give variables to the grid cells like this:
-	 *   a b c d
-	 *   e f g h
-	 *   i j k l
-	 *   m n o p
-	 * 
-	 * Let the sum of any line be s = a + b + c + d = e + f + g + h = etc. = a + f + k + p = etc.
-	 * Now do some algebra:
-	 *    s = d + h + l + p
-	 *      = (s - a - b - c) + (s - e - f - g) + (s - i - j - k) + (s - a - f - k)
-	 *      = 4s + a + b + c + e + f + g + i + j + k + a + f + k.
-	 *   3s = 2a + b + c + e + 2f + g + i + j + 2k.
-	 * Therefore the right-hand side of the last equation must be a multiple of 3.
+	 *   [ a b c d ]
+	 *   [ e f g h ]
+	 *   [ i j k l ]
+	 *   [ m n o p ]
+	 * There are 10 four-element lines in the grid. Assume each line has a sum of s.
+	 * We write 9 equations to equate the first row's sum with each other line's sum:
+	 *   a + b + c + d  =  e + f + g + h.  (1st row = 2nd row)
+	 *   a + b + c + d  =  i + j + k + l.  (1st row = 3rd row)
+	 *   a + b + c + d  =  m + n + o + p.  (1st row = 4th row)
+	 *   a + b + c + d  =  a + e + i + m.  (1st row = 1st column)
+	 *   a + b + c + d  =  b + f + j + n.  (1st row = 2nd column)
+	 *   a + b + c + d  =  c + g + k + o.  (1st row = 3rd column)
+	 *   a + b + c + d  =  d + h + l + p.  (1st row = 4th column)
+	 *   a + b + c + d  =  a + f + k + p.  (1st row = diagonal)
+	 *   a + b + c + d  =  d + g + j + m.  (1st row = antidiagonal)
+	 * Actually only 8 of these equations are linearly independent.
+	 * So with 16 variables and 8 equations, we have 8 free variables.
+	 * Suppose we know the values of {a,b,c,d,e,g,i,k}. We perform some algebra:
+	 *   m = b + c + d - e - i.  (based on 1st column)
+	 *   o = a + b + d - g - k.  (based on 3rd column)
+	 *   j = a + b + c - g - m.  (based on antidiagonal)
+	 *   l = a + b + c + d - i - j - k.  (based on 3rd row)
+	 * Now comes a difficult step. We need to solve a full system of linear equations to get the rest of the values.
+	 * Here I state the result for f and show its correctness, but not explain how the solution was found:
+	 *   - 1 (a + b + c + d)     - 1 (m + n + o + p)
+	 *   + 2 (a + b + c + d)     + 2 (a + e + i + m)
+	 *   + 1 (a + b + c + d)  =  + 1 (b + f + j + n)  .
+	 *   + 1 (a + b + c + d)     + 1 (c + g + k + o)
+	 *   + 1 (a + b + c + d)     + 1 (a + f + k + p)
+	 *   - 1 (a + b + c + d)     - 1 (d + g + j + m)
+	 * Adding up all terms on the left side and on the right side, and simplifying, we get:
+	 *   3(a + b + c + d) = 3a + 2e + 2i + b + 2f + c + 2k - d.
+	 *   f = b + c + 2d - e - i - k.
+	 * The rest of the way is smooth sailing:
+	 *   h = a + b + c + d - e - f - g.  (based on 2nd row)
+	 *   n = a + c + d - f - j.  (based on 2nd column)
+	 *   p = a + b + c - h - l.  (based on 4th column)
 	 */
 	public String run() {
 		int count = 0;
-		int[] num = new int[9];  // A counter in base 10, little-endian
+		int[] num = new int[8];  // A counter in base 10, little-endian
 		do {
-			/* 
-			 * Grid guide:
-			 *   0 1 2 a
-			 *   3 4 5 b
-			 *   6 7 8 c
-			 *   d e f g
-			 */
-			int sum = num[0]*2 + num[1] + num[2] + num[3] + num[4]*2 + num[5] + num[6] + num[7] + num[8]*2;
-			if (sum % 3 != 0) continue;
-			sum /= 3;  // The sum of each line
-			int a = sum - num[0] - num[1] - num[2];  if (a < 0 || a > 9) continue;
-			int b = sum - num[3] - num[4] - num[5];  if (b < 0 || b > 9) continue;
-			int c = sum - num[6] - num[7] - num[8];  if (c < 0 || c > 9) continue;
-			int d = sum - num[0] - num[3] - num[6];  if (d < 0 || d > 9) continue;
-			int e = sum - num[1] - num[4] - num[7];  if (e < 0 || e > 9) continue;
-			int f = sum - num[2] - num[5] - num[8];  if (f < 0 || f > 9) continue;
-			int g = sum - d - e - f;  if (g < 0 || g > 9) continue;
-			if (a + num[5] + num[7] + d != sum || num[0] + num[4] + num[8] + g != sum) continue;
-			// No need to test 'a + b + c + g == sum' because it's algebraically true
-			count++;
+			int a = num[0], b = num[1], c = num[2], d = num[3];
+			int e = num[4], g = num[5], i = num[6], k = num[7];
+			int m = b + c + d - e - i;          if (m < 0 || m > 9) continue;
+			int o = a + b + d - g - k;          if (o < 0 || o > 9) continue;
+			int j = a + b + c - g - m;          if (j < 0 || j > 9) continue;
+			int l = a + b + c + d - i - j - k;  if (l < 0 || l > 9) continue;
+			int f = b + c + d*2 - e - i - k;    if (f < 0 || f > 9) continue;
+			int h = a + b + c + d - e - f - g;  if (h < 0 || h > 9) continue;
+			int n = a + c + d - f - j;          if (n < 0 || n > 9) continue;
+			int p = a + b + c - h - l;          if (p < 0 || p > 9) continue;
+			count++;  // All tests passed
 		} while (increment(num));
 		return Integer.toString(count);
 	}
 	
 	
-	// Increments the given base-10 counter in little endian, e.g. {9, 5, 1} -> {0, 6, 1}.
+	// Increments the given base-10 counter in little endian, e.g. {9, 9, 5, 1} -> {0, 0, 6, 1}.
 	// Returns true if incremented, otherwise returns false if all elements are already 9.
 	private static boolean increment(int[] num) {
 		int i = 0;
--- python/p166.py
+++ python/p166.py
@@ -8,65 +8,72 @@
 
 
 # Give variables to the grid cells like this:
-#   a b c d
-#   e f g h
-#   i j k l
-#   m n o p
-# 
-# Let the sum of any line be s = a + b + c + d = e + f + g + h = etc. = a + f + k + p = etc.
-# Now do some algebra:
-#    s = d + h + l + p
-#      = (s - a - b - c) + (s - e - f - g) + (s - i - j - k) + (s - a - f - k)
-#      = 4s + a + b + c + e + f + g + i + j + k + a + f + k.
-#   3s = 2a + b + c + e + 2f + g + i + j + 2k.
-# Therefore the right-hand side of the last equation must be a multiple of 3.
+#   [ a b c d ]
+#   [ e f g h ]
+#   [ i j k l ]
+#   [ m n o p ]
+# There are 10 four-element lines in the grid. Assume each line has a sum of s.
+# We write 9 equations to equate the first row's sum with each other line's sum:
+#   a + b + c + d  =  e + f + g + h.  (1st row = 2nd row)
+#   a + b + c + d  =  i + j + k + l.  (1st row = 3rd row)
+#   a + b + c + d  =  m + n + o + p.  (1st row = 4th row)
+#   a + b + c + d  =  a + e + i + m.  (1st row = 1st column)
+#   a + b + c + d  =  b + f + j + n.  (1st row = 2nd column)
+#   a + b + c + d  =  c + g + k + o.  (1st row = 3rd column)
+#   a + b + c + d  =  d + h + l + p.  (1st row = 4th column)
+#   a + b + c + d  =  a + f + k + p.  (1st row = diagonal)
+#   a + b + c + d  =  d + g + j + m.  (1st row = antidiagonal)
+# Actually only 8 of these equations are linearly independent.
+# So with 16 variables and 8 equations, we have 8 free variables.
+# Suppose we know the values of {a,b,c,d,e,g,i,k}. We perform some algebra:
+#   m = b + c + d - e - i.  (based on 1st column)
+#   o = a + b + d - g - k.  (based on 3rd column)
+#   j = a + b + c - g - m.  (based on antidiagonal)
+#   l = a + b + c + d - i - j - k.  (based on 3rd row)
+# Now comes a difficult step. We need to solve a full system of linear equations to get the rest of the values.
+# Here I state the result for f and show its correctness, but not explain how the solution was found:
+#   - 1 (a + b + c + d)     - 1 (m + n + o + p)
+#   + 2 (a + b + c + d)     + 2 (a + e + i + m)
+#   + 1 (a + b + c + d)  =  + 1 (b + f + j + n)  .
+#   + 1 (a + b + c + d)     + 1 (c + g + k + o)
+#   + 1 (a + b + c + d)     + 1 (a + f + k + p)
+#   - 1 (a + b + c + d)     - 1 (d + g + j + m)
+# Adding up all terms on the left side and on the right side, and simplifying, we get:
+#   3(a + b + c + d) = 3a + 2e + 2i + b + 2f + c + 2k - d.
+#   f = b + c + 2d - e - i - k.
+# The rest of the way is smooth sailing:
+#   h = a + b + c + d - e - f - g.  (based on 2nd row)
+#   n = a + c + d - f - j.  (based on 2nd column)
+#   p = a + b + c - h - l.  (based on 4th column)
 def compute():
 	ans = 0
-	num = [0] * 9  # A counter in base 10, little-endian
-	keepgoing = True
-	while keepgoing:
-		while True:  # A single-iteration loop to let us simulate goto
-			# Grid guide:
-			#   0 1 2 a
-			#   3 4 5 b
-			#   6 7 8 c
-			#   d e f g
-			sum = num[0]*2 + num[1] + num[2] + num[3] + num[4]*2 + num[5] + num[6] + num[7] + num[8]*2
-			if sum % 3 != 0: break
-			sum //= 3  # The sum of each line
-			a = sum - num[0] - num[1] - num[2]
-			if a < 0 or a > 9: break
-			b = sum - num[3] - num[4] - num[5]
-			if b < 0 or b > 9: break
-			c = sum - num[6] - num[7] - num[8]
-			if c < 0 or c > 9: break
-			d = sum - num[0] - num[3] - num[6]
-			if d < 0 or d > 9: break
-			e = sum - num[1] - num[4] - num[7]
-			if e < 0 or e > 9: break
-			f = sum - num[2] - num[5] - num[8]
-			if f < 0 or f > 9: break
-			g = sum - d - e - f
-			if g < 0 or g > 9: break
-			if (a + num[5] + num[7] + d) != sum or (num[0] + num[4] + num[8] + g) != sum: break
-			# No need to test 'a + b + c + g == sum' because it's algebraically true
-			ans += 1
-			break
-		keepgoing = increment(num)
+	digits = tuple(range(10))
+	for b in digits:
+		for c in digits:
+			for d in digits:
+				for e in digits:
+					for i in digits:
+						m = b + c + d - e - i
+						if m < 0 or m > 9: continue
+						for k in digits:
+							f = b + c + d*2 - e - i - k
+							if f < 0 or f > 9: continue
+							for a in digits:
+								for g in digits:
+									o = a + b + d - g - k
+									if o < 0 or o > 9: continue
+									j = a + b + c - g - m
+									if j < 0 or j > 9: continue
+									l = a + b + c + d - i - j - k
+									if l < 0 or l > 9: continue
+									h = a + b + c + d - e - f - g
+									if h < 0 or h > 9: continue
+									n = a + c + d - f - j
+									if n < 0 or n > 9: continue
+									p = a + b + c - h - l
+									if p < 0 or p > 9: continue
+									ans += 1
 	return str(ans)
-
-
-# Increments the given base-10 counter in little endian, e.g. [9, 5, 1] -> [0, 6, 1].
-# Returns true if incremented, otherwise returns false if all elements are already 9.
-def increment(num):
-	i = 0
-	while num[i] == 9:
-		num[i] = 0
-		i += 1
-		if i == len(num):
-			return False
-	num[i] += 1
-	return True
 
 
 if __name__ == "__main__":
