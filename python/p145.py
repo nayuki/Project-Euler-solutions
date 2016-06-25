73ec1a1f5a9fdf5569f22aed0ecd7f37e04ce2eb
Project Nayuki <me@nayuki.io>
2016-06-25 06:36:32 +0000

    P145: Sped up Python solution by significantly rewriting algorithm.

--- python/p145.py
+++ python/p145.py
@@ -7,19 +7,32 @@
 # 
 
 
-# A slow brute-force answer.
+# A slow, mostly brute-force answer.
 def compute():
 	ODD_DIGITS = {"1", "3", "5", "7", "9"}
 	
-	def is_sum_reversible_odd(x):
-		xstr = str(x)
-		if xstr[-1] == "0":
-			return False  # The reverse can't have leading zero
-		revx = int(xstr[::-1])
+	# This requires x mod 10 != 0.
+	def is_reversible(x):
+		revx = int(str(x)[::-1])
 		sumstr = str(x + revx)
 		return all(c in ODD_DIGITS for c in sumstr)
 	
-	ans = sum(1 for i in range(10**9) if is_sum_reversible_odd(i))
+	ans = 0
+	for digits in range(2, 10):  # Note: There are no 1-digit reversible numbers
+		for i in range(1, 10):  # For each leading digit
+			start = (i + 0) * (10**(digits - 1))
+			end   = (i + 1) * (10**(digits - 1))
+			# Split into cases so that the sum of the first digit and
+			# last digit is always odd, thus saving about half the work
+			if i % 2 == 0:  # So try numbers ending in odd digit
+				for j in range(start + 1, end, 2):
+					ans += int(is_reversible(j))
+			else:  # i % 2 == 1, so try numbers ending in even non-zero digit
+				for j in range(start, end, 10):
+					ans += int(is_reversible(j + 2))
+					ans += int(is_reversible(j + 4))
+					ans += int(is_reversible(j + 6))
+					ans += int(is_reversible(j + 8))
 	return str(ans)
 
 
