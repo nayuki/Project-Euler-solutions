afd595b6bf92c3e72779c848bea80c10920ce699
Project Nayuki <me@nayuki.io>
2016-12-26 05:19:56 +0000

    P518: Optimized Java solution further, added mathematical proof, added Python solution.

--- java/p518.java
+++ java/p518.java
@@ -6,11 +6,6 @@
  * https://github.com/nayuki/Project-Euler-solutions
  */
 
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
-
 
 public final class p518 implements EulerSolution {
 	
@@ -19,25 +14,59 @@ public final class p518 implements EulerSolution {
 	}
 	
 	
+	/* 
+	 * Suppose (a+1, b+1, c+1) are three positive integers that form a geometric sequence.
+	 * Then by definition, (c+1)/(b+1) = (b+1)/(a+1) = r for some ratio r.
+	 * r is a rational number because we are given that a, b, c are integers.
+	 * 
+	 * Because of this, we can express r = z / y in lowest terms (i.e. y and z are coprime).
+	 * Note that since the solution requires a < b < c, we require r > 1, hence z > y > 0.
+	 * 
+	 * Let's define x = (a+1) / y^2. Then we rearrange to get a + 1 = x * y * y.
+	 * We argue that x is an integer. Look at (c+1)/(a+1) = z^2/y^2. y^2 and z^2 are coprime,
+	 * thus the simplified denominator y^2 must divide the original denominator of a+1.
+	 * 
+	 * With {x, y, z} defined, we have b + 1 = x * y * z and c + 1 = x * z * z.
+	 * Therefore every possible solution (a+1, b+1, c+1) can be re-expressed as
+	 * a triple of integers (x, y, z) such that x >= 1, y >= 1, z > y, and gcd(y,z) = 1.
+	 * In fact, this mapping of (a+1, b+1, c+1) to (x, y, z) is unique - this is because
+	 * the ratio (b+1)/(a+1) uniquely determines y and z; subsequently a and y together give x.
+	 * 
+	 * The rest of the algorithm is a matter of searching x, y, z in some ascending order, and stopping
+	 * each loop when no more candidates are possible because they all necessarily exceed the limit.
+	 */
+	
 	private static final int LIMIT = Library.pow(10, 8);
 	
 	public String run() {
 		long sum = 0;
 		boolean[] isPrime = Library.listPrimality(LIMIT - 1);
-		for (int b = 2; b < isPrime.length; b++) {
-			if (!isPrime[b])
-				continue;
+		
+		// Search all possible x's. Note that a + 1 = x * y * y >= x. Furthermore, a < b, so a + 1 <= b.
+		// Thus if x >= LIMIT, then LIMIT <= a + 1 <= b. With b >= LIMIT, no candidates are possible.
+		for (int x = 1; x < isPrime.length; x++) {
 			
-			List<Integer> factors = getFactors(b + 1);
-			Set<Long> foundA = new HashSet<>();
-			for (int u : factors) {
-				for (int v : factors) {
-					if (u <= v)
+			// Search all possible y's. Notice that when y increases, 'a' strictly increases.
+			// So when some y generates an 'a' such that a >= LIMIT, no candidates are possible with higher values of y.
+			for (int y = 1; ; y++) {
+				long a = (long)x * y * y - 1;
+				if (a >= isPrime.length)
+					break;
+				if (!isPrime[(int)a])
+					continue;
+				
+				// Search all valid z's. We require z > y and gcd(y, z) = 1. Notice that when z increases, c strictly increases.
+				// So when some z generates a c such that c >= LIMIT, no candidates are possible with higher values of z.
+				for (int z = y + 1; ; z++) {
+					if (Library.gcd(y, z) != 1)
 						continue;
+					long b = (long)x * y * z - 1;
+					long c = (long)x * z * z - 1;
+					if (c >= isPrime.length)
+						break;
 					
-					long a = (b + 1L) / u * v - 1;
-					long c = (b + 1L) / v * u - 1;
-					if (a < isPrime.length && c < isPrime.length && isPrime[(int)a] && isPrime[(int)c] && foundA.add(a)) {
+					// Check whether (a, b, c) is a solution
+					if (isPrime[(int)b] && isPrime[(int)c]) {
 						long addend = a + b + c;
 						if (sum + addend < sum)
 							throw new ArithmeticException("Overflow");
@@ -47,21 +76,6 @@ public final class p518 implements EulerSolution {
 			}
 		}
 		return Long.toString(sum);
-	}
-	
-	
-	private static List<Integer> getFactors(int n) {
-		if (n <= 0)
-			throw new IllegalArgumentException();
-		List<Integer> result = new ArrayList<>();
-		for (int i = 1, end = Library.sqrt(n); i <= end; i++) {
-			if (n % i == 0) {
-				result.add(i);
-				if (i != end)
-					result.add(n / i);
-			}
-		}
-		return result;
 	}
 	
 }
---
+++ python/p518.py
@@ -0,0 +1,68 @@
+# 
+# Solution to Project Euler problem 518
+# by Project Nayuki
+# 
+# https:#www.nayuki.io/page/project-euler-solutions
+# https:#github.com/nayuki/Project-Euler-solutions
+# 
+
+import eulerlib, fractions, itertools
+
+
+# Suppose (a+1, b+1, c+1) are three positive integers that form a geometric sequence.
+# Then by definition, (c+1)/(b+1) = (b+1)/(a+1) = r for some ratio r.
+# r is a rational number because we are given that a, b, c are integers.
+# 
+# Because of this, we can express r = z / y in lowest terms (i.e. y and z are coprime).
+# Note that since the solution requires a < b < c, we require r > 1, hence z > y > 0.
+# 
+# Let's define x = (a+1) / y^2. Then we rearrange to get a + 1 = x * y * y.
+# We argue that x is an integer. Look at (c+1)/(a+1) = z^2/y^2. y^2 and z^2 are coprime,
+# thus the simplified denominator y^2 must divide the original denominator of a+1.
+# 
+# With {x, y, z} defined, we have b + 1 = x * y * z and c + 1 = x * z * z.
+# Therefore every possible solution (a+1, b+1, c+1) can be re-expressed as
+# a triple of integers (x, y, z) such that x >= 1, y >= 1, z > y, and gcd(y,z) = 1.
+# In fact, this mapping of (a+1, b+1, c+1) to (x, y, z) is unique - this is because
+# the ratio (b+1)/(a+1) uniquely determines y and z; subsequently a and y together give x.
+# 
+# The rest of the algorithm is a matter of searching x, y, z in some ascending order, and stopping
+# each loop when no more candidates are possible because they all necessarily exceed the limit.
+def compute():
+	LIMIT = 10**8
+	
+	ans = 0
+	isprime = eulerlib.list_primality(LIMIT - 1)
+	
+	# Search all possible x's. Note that a + 1 = x * y * y >= x. Furthermore, a < b, so a + 1 <= b.
+	# Thus if x >= LIMIT, then LIMIT <= a + 1 <= b. With b >= LIMIT, no candidates are possible.
+	for x in range(1, LIMIT):
+		
+		# Search all possible y's. Notice that when y increases, 'a' strictly increases.
+		# So when some y generates an 'a' such that a >= LIMIT, no candidates are possible with higher values of y.
+		for y in itertools.count(1):
+			a = x * y * y - 1
+			if a >= LIMIT:
+				break
+			if not isprime[a]:
+				continue
+			
+			# Search all valid z's. We require z > y and gcd(y, z) = 1. Notice that when z increases, c strictly increases.
+			# So when some z generates a c such that c >= LIMIT, no candidates are possible with higher values of z.
+			for z in itertools.count(y + 1):
+				if fractions.gcd(y, z) != 1:
+					continue
+				c = x * z * z - 1
+				if c >= LIMIT:
+					break
+				
+				# Check whether (a, b, c) is a solution
+				b = x * y * z - 1
+				if isprime[b] and isprime[c]:
+					ans += a + b + c
+	
+	return str(ans)
+
+
+if __name__ == "__main__":
+	print(compute())
