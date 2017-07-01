9c8e3e354840f7df77b38a02fb97425cbf148df2
Project Nayuki <me@nayuki.io>
2017-07-01 19:12:11 +0000

    P208: Updated explanation and code in Java and Python solutions for improved clarity and simplicity.

--- java/p208.java
+++ java/p208.java
@@ -21,81 +21,93 @@ public final class p208 implements EulerSolution {
 	
 	/* 
 	 * Because the robot moves in 72-degree arcs, it can only face 5 possible directions.
-	 * - Let the initial northward direction be defined as the integer 0.
-	 * - Let each anticlockwise (leftward) move add 1 from the direction, modulo 5.
-	 * - Let each clockwise (rightward) move subtract 1 to the direction, modulo 5.
+	 * - Define the initial northward direction as the integer 0.
+	 * - Let each anticlockwise (leftward) move add 1 to the direction, modulo 5.
+	 * - Let each clockwise (rightward) move subtract 1 from the direction, modulo 5.
 	 * 
-	 * There are 5 possible direction states, and 2 possible moves from each state.
+	 * At each of the 5 possible facing directions, there are 2 possible moves.
 	 * What is the (x, y) displacement of each of these 10 possible moves?
 	 * 
-	 * We can figure this out by drawing a unit circle with 5 points evenly
-	 * spaced 72 degrees apart, such that one point is on the positive x axis.
-	 * Doing some moderate algebra and trigonometry, we get these coordinates:
-	 * - Point 0: (cos   0, sin   0) = (+1, 0).
-	 * - Point 1: (cos  72, sin  72) = (+(sqrt(5)-1)/4, +sqrt(10+2sqrt(5))/4).
-	 * - Point 2: (cos 144, sin 144) = (-(sqrt(5)+1)/4, +sqrt(10-2sqrt(5))/4).
-	 * - Point 3: (cos 216, sin 216) = (-(sqrt(5)+1)/4, -sqrt(10-2sqrt(5))/4).
-	 * - Point 4: (cos 288, sin 288) = (+(sqrt(5)-1)/4, -sqrt(10+2sqrt(5))/4).
+	 * We can figure this out by drawing a circle with 5 points evenly spaced 72
+	 * degrees apart. To align with the problem description, one of the points will
+	 * be on the positive x axis. For our convenience, the circle shall have radius 4.
+	 * Doing some moderate algebra and trigonometry, we get these point coordinates:
+	 * - Point 0: (cos   0, sin   0)*4 = (+4, 0).
+	 * - Point 1: (cos  72, sin  72)*4 = (+(sqrt(5)-1), +sqrt(10+2sqrt(5))).
+	 * - Point 2: (cos 144, sin 144)*4 = (-(sqrt(5)+1), +sqrt(10-2sqrt(5))).
+	 * - Point 3: (cos 216, sin 216)*4 = (-(sqrt(5)+1), -sqrt(10-2sqrt(5))).
+	 * - Point 4: (cos 288, sin 288)*4 = (+(sqrt(5)-1), -sqrt(10+2sqrt(5))).
 	 * (The inputs to cos and sin are given in degrees.)
 	 * 
-	 * This sequence of points is constructed so that the displacement vector from point k to point
-	 * k+1 is equal to the displacement of making a anticlockwise move when facing direction k.
-	 * For example, (point1 - point0) is the displacement of an anticlockwise move when facing north (dir=0).
-	 * The actual displacement values are:
-	 * - point1 - point0 = (+(sqrt(5)-5)/4, +sqrt(10+2sqrt(5))/4).
-	 * - point2 - point1 = (-sqrt(5)/2, -sqrt(5-2sqrt(5))/2).
-	 * - point3 - point2 = (0, -sqrt(10-2sqrt(5))/2).
-	 * - point4 - point3 = (+sqrt(5)/2, -sqrt(5-2sqrt(5))/2).
-	 * - point0 - point4 = (-(sqrt(5)-5)/4, +sqrt(10+2sqrt(5))/4).
+	 * This sequence of points is constructed so that the displacement vector from
+	 * point k to point k+1 is equal to the displacement of making an anticlockwise
+	 * move when facing direction k. For example, (point1 - point0) is the
+	 * displacement of an anticlockwise move when facing north (dir=0).
+	 * Hence, the displacement vectors of the 5 anticlockwise move are:
+	 * - point1 - point0 = (-(5-sqrt(5)),  +sqrt(10+2sqrt(5))).
+	 * - point2 - point1 = (-2sqrt(5)   , -2sqrt( 5-2sqrt(5))).
+	 * - point3 - point2 = (0           , -2sqrt(10-2sqrt(5))).
+	 * - point4 - point3 = (+2sqrt(5)   , -2sqrt( 5-2sqrt(5))).
+	 * - point0 - point4 = (+(5-sqrt(5)),  +sqrt(10+2sqrt(5))).
 	 * As for clockwise moves, simply take the displacements vectors above and negate the x values.
 	 * The mapping of displacement vectors to direction states also needs to be negated modulo 5.
 	 * 
 	 * Altogether, we have this table of valid moves:
 	 *    Direction | Move | x displacement |   y displacement
 	 *   -----------+------+----------------+---------------------
-	 *        0     | ACW  | +(sqrt(5)-5)/4 | +sqrt(10+2sqrt(5))/4
-	 *        0     |  CW  | -(sqrt(5)-5)/4 | +sqrt(10+2sqrt(5))/4
-	 *        1     | ACW  |   -sqrt(5)/2   | -sqrt(5-2sqrt(5))/2
-	 *        1     |  CW  | +(sqrt(5)-5)/4 | -sqrt(5-2sqrt(5))/2
-	 *        2     | ACW  |       0        | -sqrt(10-2sqrt(5))/2
-	 *        2     |  CW  |   -sqrt(5)/2   | -sqrt(5-2sqrt(5))/2
-	 *        3     | ACW  |   +sqrt(5)/2   | -sqrt(5-2sqrt(5))/2
-	 *        3     |  CW  |       0        | -sqrt(10-2sqrt(5))/2
-	 *        4     | ACW  | -(sqrt(5)-5)/4 | -sqrt(5-2sqrt(5))/2
-	 *        4     |  CW  |   +sqrt(5)/2   | -sqrt(5-2sqrt(5))/2
+	 *        0     | ACW  |  -(5-sqrt(5))  | +sqrt(10+2sqrt(5))
+	 *        0     |  CW  |  +(5-sqrt(5))  | +sqrt(10+2sqrt(5))
+	 *        1     | ACW  |    -2sqrt(5)   | -2sqrt(5-2sqrt(5))
+	 *        1     |  CW  |  -(5-sqrt(5))  | +sqrt(10+2sqrt(5))
+	 *        2     | ACW  |        0       | -2sqrt(10-2sqrt(5))
+	 *        2     |  CW  |    -2sqrt(5)   | -2sqrt(5-2sqrt(5))
+	 *        3     | ACW  |    +2sqrt(5)   | -2sqrt(5-2sqrt(5))
+	 *        3     |  CW  |        0       | -2sqrt(10-2sqrt(5))
+	 *        4     | ACW  |  +(5-sqrt(5))  | +sqrt(10+2sqrt(5))
+	 *        4     |  CW  |    +2sqrt(5)   | -2sqrt(5-2sqrt(5))
+	 * Note that -2sqrt(5-2sqrt(5)) = sqrt(10-2sqrt(5)) - sqrt(10+2sqrt(5)).
 	 * 
-	 * As the robot moves, it adds up x components and y components to its displacement.
-	 * - At any given time, the x coordinate can be described as a unique weighted integer
-	 *   sum of sqrt(5)/2 and (sqrt(5)-5)/4, namely i*sqrt(5)/2 + j*(sqrt(5)-5)/4.
-	 *   We could prove that these two irrational components cannot "simplify"
+	 * As the robot moves, it adds x components and y components to its displacement.
+	 * - At any given time, the x coordinate equals a unique integer-weighted
+	 *   sum of 2sqrt(5) and (sqrt(5)-5), namely i*2sqrt(5) + j*(sqrt(5)-5).
+	 *   We can show that these two irrational components cannot "simplify"
 	 *   with each other - i.e. when (i, j) != (0, 0), the sum must be non-zero.
-	 * - At any given time, the y coordinate can be described the dot product of
-	 *   the vector (-sqrt(10-2sqrt(5))/2, sqrt(10+2sqrt(5))/4, -sqrt(5-2sqrt(5))/2)
-	 *   with some particular vector of 3 natural numbers.
-	 *   Note very importantly that there is a cancellation property in effect:
-	 *   1*-sqrt(10-2sqrt(5))/2 + 2*+sqrt(10+2sqrt(5))/4 + 2*-sqrt(5-2sqrt(5))/2 = 0.
+	 * - At any given time, the y coordinate equals a unique integer-weighted
+	 *   sum of sqrt(10-2sqrt(5)) and sqrt(10+2sqrt(5)). We will assume without proof that
+	 *   these two components don't "interact" with each other or cancel out in any way.
 	 */
 	
 	private static final int LIMIT = 70;
 	
 	public String run() {
+		// Start with the initial state being reachable
 		Map<State,BigInteger> reachable = new HashMap<>();
-		reachable.put(new State(0, 0, 0, 0, 0, 0), BigInteger.ONE);
+		reachable.put(new State(0, 0, 0, 0, 0), BigInteger.ONE);
 		
+		// In each iteration, look at the set of reachable states and apply ACW and CW
+		// moves on each state. Count the number of ways the new states can be reached.
 		for (int i = 0; i < LIMIT; i++) {
 			Map<State,BigInteger> newReachable = new HashMap<>();
 			for (Map.Entry<State,BigInteger> entry : reachable.entrySet()) {
 				State state = entry.getKey();
 				BigInteger ways = entry.getValue();
-				add(newReachable, state.moveAnticlockwise(), ways);
-				add(newReachable, state.moveClockwise    (), ways);
+				
+				State acw = state.move(true);
+				BigInteger temp = newReachable.containsKey(acw) ? newReachable.get(acw) : BigInteger.ZERO;
+				newReachable.put(acw, temp.add(ways));
+				
+				State cw = state.move(false);
+				temp = newReachable.containsKey(cw) ? newReachable.get(cw) : BigInteger.ZERO;
+				newReachable.put(cw, temp.add(ways));
 			}
 			reachable = newReachable;
 		}
 		
+		// Add up all the ways to reach the initial position using
+		// exactly LIMIT moves, finally facing any possible direction
 		BigInteger sum = BigInteger.ZERO;
 		for (int dir = 0; dir < 5; dir++) {
-			State st = new State(dir, 0, 0, 0, 0, 0);
+			State st = new State(dir, 0, 0, 0, 0);
 			if (reachable.containsKey(st))
 				sum = sum.add(reachable.get(st));
 		}
@@ -103,59 +115,38 @@ public final class p208 implements EulerSolution {
 	}
 	
 	
-	private static void add(Map<State,BigInteger> reachable, State state, BigInteger ways) {
-		BigInteger temp = reachable.containsKey(state) ? reachable.get(state) : BigInteger.ZERO;
-		reachable.put(state, temp.add(ways));
-	}
 	
-	
-	
+	// Represents a facing-direction and a position on the infinite plane.
+	// The position is a weighted sum of irrational x components and y components.
 	private static final class State {
 		
 		public final int direction;  // In the range [0, 5).
-		
-		public final int xComponent0;  // Any integer. For sqrt(5)/2.
-		public final int xComponent1;  // Any integer. For (sqrt(5)-5)/4.
-		
-		public final int yComponent0;  // Non-negative. For -sqrt(10-2sqrt(5))/2.
-		public final int yComponent1;  // Non-negative. For +sqrt(10+2sqrt(5))/4.
-		public final int yComponent2;  // Non-negative. For -sqrt(5-2sqrt(5))/2.
+		public final int xComponent0;  // Any integer. For 2sqrt(5).
+		public final int xComponent1;  // Any integer. For sqrt(5)-5.
+		public final int yComponent0;  // Any integer. For sqrt(10-2sqrt(5)).
+		public final int yComponent1;  // Any integer. For sqrt(10+2sqrt(5)).
 		
 		
-		public State(int dir, int xComp0, int xComp1, int yComp0, int yComp1, int yComp2) {
+		public State(int dir, int xComp0, int xComp1, int yComp0, int yComp1) {
+			if (dir < 0 || dir >= 5)
+				throw new IllegalArgumentException();
 			direction = dir;
 			xComponent0 = xComp0;
 			xComponent1 = xComp1;
-			while (yComp0 >= 1 && yComp1 >= 2 && yComp2 >= 2) {
-				yComp0 -= 1;
-				yComp1 -= 2;
-				yComp2 -= 2;
-			}
 			yComponent0 = yComp0;
 			yComponent1 = yComp1;
-			yComponent2 = yComp2;
 		}
 		
 		
-		public State moveAnticlockwise() {
+		public State move(boolean anticlockwise) {
+			int sign = anticlockwise ? +1 : -1;
+			int[] entry = ANTICLOCKWISE_MOVES[(direction * sign + 5) % 5];
 			return new State(
-				(direction + 1) % 5,
-				xComponent0 + MOVE_TABLE[direction][0][0],
-				xComponent1 + MOVE_TABLE[direction][0][1],
-				yComponent0 + MOVE_TABLE[direction][0][2],
-				yComponent1 + MOVE_TABLE[direction][0][3],
-				yComponent2 + MOVE_TABLE[direction][0][4]);
-		}
-		
-		
-		public State moveClockwise() {
-			return new State(
-				(direction + 4) % 5,
-				xComponent0 + MOVE_TABLE[direction][1][0],
-				xComponent1 + MOVE_TABLE[direction][1][1],
-				yComponent0 + MOVE_TABLE[direction][1][2],
-				yComponent1 + MOVE_TABLE[direction][1][3],
-				yComponent2 + MOVE_TABLE[direction][1][4]);
+				(direction + sign + 5) % 5,
+				xComponent0 + entry[0] * sign,
+				xComponent1 + entry[1] * sign,
+				yComponent0 + entry[2],
+				yComponent1 + entry[3]);
 		}
 		
 		
@@ -168,22 +159,23 @@ public final class p208 implements EulerSolution {
 				xComponent0 == other.xComponent0 &&
 				xComponent1 == other.xComponent1 &&
 				yComponent0 == other.yComponent0 &&
-				yComponent1 == other.yComponent1 &&
-				yComponent2 == other.yComponent2;
+				yComponent1 == other.yComponent1;
 		}
 		
 		
 		public int hashCode() {
-			return Objects.hash(direction, xComponent0, xComponent1, yComponent0, yComponent1, yComponent2);
+			return Objects.hash(direction,
+				xComponent0, xComponent1,
+				yComponent0, yComponent1);
 		}
 		
 		
-		private static final int[][][] MOVE_TABLE = {
-			{{ 0, +1, 0, 1, 0}, { 0, -1, 0, 1, 0}},
-			{{-1,  0, 0, 0, 1}, { 0, +1, 0, 1, 0}},
-			{{ 0,  0, 1, 0, 0}, {-1,  0, 0, 0, 1}},
-			{{+1,  0, 0, 0, 1}, { 0,  0, 1, 0, 0}},
-			{{ 0, -1, 0, 1, 0}, {+1,  0, 0, 0, 1}},
+		private static final int[][] ANTICLOCKWISE_MOVES = {
+			{ 0, -1,  0, +1},
+			{-1,  0, +1, -1},
+			{ 0,  0, -2,  0},
+			{+1,  0, +1, -1},
+			{ 0, +1,  0, +1},
 		};
 		
 	}
--- python/p208.py
+++ python/p208.py
@@ -7,45 +7,94 @@
 # 
 
 
+# Because the robot moves in 72-degree arcs, it can only face 5 possible directions.
+# - Define the initial northward direction as the integer 0.
+# - Let each anticlockwise (leftward) move add 1 to the direction, modulo 5.
+# - Let each clockwise (rightward) move subtract 1 from the direction, modulo 5.
+# 
+# At each of the 5 possible facing directions, there are 2 possible moves.
+# What is the (x, y) displacement of each of these 10 possible moves?
+# 
+# We can figure this out by drawing a circle with 5 points evenly spaced 72
+# degrees apart. To align with the problem description, one of the points will
+# be on the positive x axis. For our convenience, the circle shall have radius 4.
+# Doing some moderate algebra and trigonometry, we get these point coordinates:
+# - Point 0: (cos   0, sin   0)*4 = (+4, 0).
+# - Point 1: (cos  72, sin  72)*4 = (+(sqrt(5)-1), +sqrt(10+2sqrt(5))).
+# - Point 2: (cos 144, sin 144)*4 = (-(sqrt(5)+1), +sqrt(10-2sqrt(5))).
+# - Point 3: (cos 216, sin 216)*4 = (-(sqrt(5)+1), -sqrt(10-2sqrt(5))).
+# - Point 4: (cos 288, sin 288)*4 = (+(sqrt(5)-1), -sqrt(10+2sqrt(5))).
+# (The inputs to cos and sin are given in degrees.)
+# 
+# This sequence of points is constructed so that the displacement vector from
+# point k to point k+1 is equal to the displacement of making an anticlockwise
+# move when facing direction k. For example, (point1 - point0) is the
+# displacement of an anticlockwise move when facing north (dir=0).
+# Hence, the displacement vectors of the 5 anticlockwise move are:
+# - point1 - point0 = (-(5-sqrt(5)),  +sqrt(10+2sqrt(5))).
+# - point2 - point1 = (-2sqrt(5)   , -2sqrt( 5-2sqrt(5))).
+# - point3 - point2 = (0           , -2sqrt(10-2sqrt(5))).
+# - point4 - point3 = (+2sqrt(5)   , -2sqrt( 5-2sqrt(5))).
+# - point0 - point4 = (+(5-sqrt(5)),  +sqrt(10+2sqrt(5))).
+# As for clockwise moves, simply take the displacements vectors above and negate the x values.
+# The mapping of displacement vectors to direction states also needs to be negated modulo 5.
+# 
+# Altogether, we have this table of valid moves:
+#    Direction | Move | x displacement |   y displacement
+#   -----------+------+----------------+---------------------
+#        0     | ACW  |  -(5-sqrt(5))  | +sqrt(10+2sqrt(5))
+#        0     |  CW  |  +(5-sqrt(5))  | +sqrt(10+2sqrt(5))
+#        1     | ACW  |    -2sqrt(5)   | -2sqrt(5-2sqrt(5))
+#        1     |  CW  |  -(5-sqrt(5))  | +sqrt(10+2sqrt(5))
+#        2     | ACW  |        0       | -2sqrt(10-2sqrt(5))
+#        2     |  CW  |    -2sqrt(5)   | -2sqrt(5-2sqrt(5))
+#        3     | ACW  |    +2sqrt(5)   | -2sqrt(5-2sqrt(5))
+#        3     |  CW  |        0       | -2sqrt(10-2sqrt(5))
+#        4     | ACW  |  +(5-sqrt(5))  | +sqrt(10+2sqrt(5))
+#        4     |  CW  |    +2sqrt(5)   | -2sqrt(5-2sqrt(5))
+# Note that -2sqrt(5-2sqrt(5)) = sqrt(10-2sqrt(5)) - sqrt(10+2sqrt(5)).
+# 
+# As the robot moves, it adds x components and y components to its displacement.
+# - At any given time, the x coordinate equals a unique integer-weighted
+#   sum of 2sqrt(5) and (sqrt(5)-5), namely i*2sqrt(5) + j*(sqrt(5)-5).
+#   We can show that these two irrational components cannot "simplify"
+#   with each other - i.e. when (i, j) != (0, 0), the sum must be non-zero.
+# - At any given time, the y coordinate equals a unique integer-weighted
+#   sum of sqrt(10-2sqrt(5)) and sqrt(10+2sqrt(5)). We will assume without proof that
+#   these two components don't "interact" with each other or cancel out in any way.
 def compute():
 	LIMIT = 70
 	
 	# Returns a new state tuple.
-	def move(state, acw):
-		dir, xcomp0, xcomp1, ycomp0, ycomp1, ycomp2 = state
-		entry = MOVE_TABLE[dir][0 if acw else 1]
-		xcomp0 += entry[0]
-		xcomp1 += entry[1]
-		ycomp0 += entry[2]
-		ycomp1 += entry[3]
-		ycomp2 += entry[4]
-		temp = min(ycomp0, ycomp1 // 2, ycomp2 // 2)
-		ycomp0 -= temp * 1
-		ycomp1 -= temp * 2
-		ycomp2 -= temp * 2
-		dir = (dir + (1 if acw else -1)) % 5
-		return (dir, xcomp0, xcomp1, ycomp0, ycomp1, ycomp2)
+	def move(state, sign):
+		entry = ANTICLOCKWISE_MOVES[state[0] * sign % 5]
+		return (
+			(state[0] + sign) % 5,
+			state[1] + entry[0] * sign,
+			state[2] + entry[1] * sign,
+			state[3] + entry[2],
+			state[4] + entry[3])
 	
-	reachable = {(0, 0, 0, 0, 0, 0): 1}
+	reachable = {(0, 0, 0, 0, 0): 1}
 	for _ in range(LIMIT):
 		newreachable = {}
 		for (state, ways) in reachable.items():
-			acwst = move(state, True )
-			cwst  = move(state, False)
+			acwst = move(state, +1)
+			cwst  = move(state, -1)
 			newreachable[acwst] = newreachable.get(acwst, 0) + ways
 			newreachable[cwst ] = newreachable.get(cwst , 0) + ways
 		reachable = newreachable
 	
-	ans = sum(reachable.get((dir, 0, 0, 0, 0, 0), 0) for dir in range(5))
+	ans = sum(reachable.get((dir, 0, 0, 0, 0), 0) for dir in range(5))
 	return str(ans)
 
 
-MOVE_TABLE = (
-	(( 0, +1, 0, 1, 0), ( 0, -1, 0, 1, 0)),
-	((-1,  0, 0, 0, 1), ( 0, +1, 0, 1, 0)),
-	(( 0,  0, 1, 0, 0), (-1,  0, 0, 0, 1)),
-	((+1,  0, 0, 0, 1), ( 0,  0, 1, 0, 0)),
-	(( 0, -1, 0, 1, 0), (+1,  0, 0, 0, 1)),
+ANTICLOCKWISE_MOVES = (
+	( 0, -1,  0, +1),
+	(-1,  0, +1, -1),
+	( 0,  0, -2,  0),
+	(+1,  0, +1, -1),
+	( 0, +1,  0, +1),
 )
 
 
