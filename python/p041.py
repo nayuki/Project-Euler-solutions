5fece25d93b8dddcaae38e83e26b63748ba258f4
Project Nayuki <me@nayuki.io>
2017-08-28 22:37:34 +0000

    P41: Tweaked Python solution for clarity.

--- python/p041.py
+++ python/p041.py
@@ -9,18 +9,20 @@
 import eulerlib
 
 
-NONPRIME_LAST_DIGITS = {0, 2, 4, 5, 6, 8}
-
 def compute():
-	for i in range(9, 0, -1):
-		arr = list(range(i, 0, -1))
+	# Note: The only 1-digit pandigital number is 1, which is not prime. Thus we require n >= 2.
+	for n in reversed(range(2, 10)):
+		arr = list(reversed(range(1, n + 1)))
 		while True:
-			if i == 1 or arr[-1] not in NONPRIME_LAST_DIGITS:
+			if arr[-1] not in NONPRIME_LAST_DIGITS:
 				n = int("".join(str(x) for x in arr))
 				if eulerlib.is_prime(n):
 					return str(n)
 			if not prev_permutation(arr):
 				break
+	raise AssertionError()
+
+NONPRIME_LAST_DIGITS = {0, 2, 4, 5, 6, 8}
 
 
 def prev_permutation(arr):
