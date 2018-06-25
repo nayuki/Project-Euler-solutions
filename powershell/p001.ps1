2fbc0e971f81835f8b4b06c6c5148b1a474a8ddd
Besmir Zanaj [lap] <besmirzanaj@gmail.com>
2018-06-24 22:21:27 -0400

    some changes in powershell/p001.ps1

--- powershell/p001.ps1
+++ powershell/p001.ps1
@@ -1,6 +1,10 @@
 ﻿$c=0
 foreach ($i in (1..999)){
-if ($i%5 -eq 0){$c=$c+$i}
-elseif($i%3 -eq 0){$c=$c+$i}
-}
+    if ($i%5 -eq 0){
+        $c=$c+$i
+        }
+    elseif($i%3 -eq 0){
+        $c=$c+$i
+        }
+        }
 Write-Host $c
