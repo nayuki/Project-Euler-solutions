3e3adf331181bed2dfba0ed0ccb196d4e882b665
Thuong1998 <37398323+Thuong1998@users.noreply.github.com>
2019-09-27 16:58:54 +0700

    Update LibraryTest2.java

--- java/LibraryTest2.java
+++ java/LibraryTest2.java
@@ -5,12 +5,33 @@ import org.junit.jupiter.api.Test;
 class LibraryTest2 {
 
 	@Test
-	void testPow() {
+	// test with case  x = 0 ; y = 0
+	void testPow1() {
 		assertNotSame("Negative base not supported", Library.pow(0,0));
-		assertNotSame("Negative base not supported", Library.pow(0,1));
-		assertNotSame("Negative exponent", Library.pow(1,0));
-		assertEquals(1,Library.pow(1,1));
-		assertNotSame("Negative exponent", Library.pow(9999999,99999));
 	}
 
+	@Test
+	
+	// test with case  x = 0
+	void testPow2() {
+		assertNotSame("Negative base not supported", Library.pow(0,1));
+	}
+	
+	// test with case  y = 0
+	@Test
+	void testPow1() {
+		assertNotSame("Negative exponent", Library.pow(1,0));
+	}
+	
+	@Test
+	// test with case  x != 0 and y != 0
+	void testPow1() {
+		assertEquals(1,Library.pow(1,1));
+	}
+	
+	@Test
+	// test with case over flow
+	void testPow1() {
+		assertNotSame("Negative exponent", Library.pow(9999999,99999));
+	}
 }
