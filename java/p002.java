9f89d4b3af38f7f53f1fa5fb92fffe286c3fde38
Sidhant Puntambekar <sidhantnp@yahoo.com>
2019-10-02 09:30:06 -0600

    Modified problem 2

--- java/p002.java
+++ java/p002.java
@@ -21,9 +21,9 @@ public final class p002 implements EulerSolution {
 	 */
 	public String run() {
 		int sum = 0;
-		int x = 1;  // Represents the current Fibonacci number being processed
-		int y = 2;  // Represents the next Fibonacci number in the sequence
-		while (x <= 4000000) {
+		int x = 0;  // Represents the current Fibonacci number being processed
+		int y = 1;  // Represents the next Fibonacci number in the sequence
+		while (x < 4000000) {
 			if (x % 2 == 0)
 				sum += x;
 			int z = x + y;
