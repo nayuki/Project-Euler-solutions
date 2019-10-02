b3b8bb6e454e10ffb25b641ee43628e49fa5f30c
Sidhant Puntambekar <sidhantnp@yahoo.com>
2019-10-02 09:44:38 -0600

    Modified Problem 4

--- java/p004.java
+++ java/p004.java
@@ -7,27 +7,36 @@
  */
 
 
-public final class p004 implements EulerSolution {
-	
-	public static void main(String[] args) {
-		System.out.println(new p004().run());
-	}
-	
-	
-	/* 
-	 * Computers are fast, so we can implement this solution directly without any clever math.
-	 * Note that the maximum product is 999 * 999, which fits in a Java int type.
-	 */
-	public String run() {
-		int maxPalin = -1;
-		for (int i = 100; i < 1000; i++) {
-			for (int j = 100; j < 1000; j++) {
-				int prod = i * j;
-				if (Library.isPalindrome(prod) && prod > maxPalin)
-					maxPalin = prod;
-			}
-		}
-		return Integer.toString(maxPalin);
-	}
-	
+public final class p004
+{
+	public static boolean isPalindrome(int number)
+    {
+        String palindrome1 = number + "";
+        for (int i = 0; i < palindrome1.length() / 2; i++)
+        {
+            if (palindrome1.charAt(i) != palindrome1.charAt(palindrome1.length()-i-1))
+                return false;
+        }
+        return true;
+    }
+
+    public static int ReturnPalindrome()
+    {   
+        int doublePalindrome = 0;
+        int b = 0; 
+        for (int i = 999; i > 100; i--)
+        {
+            for (int j = 999; j > 100; j--)
+            {
+                if (isPalindrome(i*j) && i*j > doublePalindrome)
+                    doublePalindrome = i*j;
+            }
+        }
+        return doublePalindrome;
+    }
+
+    public static void main(String[] args)
+    {
+        System.out.println(ReturnPalindrome());
+    }
 }
