b5871cbcebf3c7ad6a753950e65e10aa6ed5b6d2
brayo-pip <62670517+brayo-pip@users.noreply.github.com>
2020-04-10 17:24:22 +0300

    Update p025.py

--- python/p025.py
+++ python/p025.py
@@ -5,28 +5,20 @@
 # https://www.nayuki.io/page/project-euler-solutions
 # https://github.com/nayuki/Project-Euler-solutions
 # 
+#https://github.com/brayo-pip/Project-Euler-solutions
+from math import sqrt,log10
 
-import itertools
-
-
-# Because the target number is relatively small, we simply compute each Fibonacci number starting
-# from the beginning until we encounter one with exactly 1000 digits. The Fibonacci sequence grows
-# exponentially with a base of about 1.618, so the numbers in base 10 will lengthen by one digit
-# after every log10(1.618) ~= 4.78 steps on average. This means the answer is at index around 4780.
 def compute():
-	DIGITS = 1000
-	prev = 1
-	cur = 0
-	for i in itertools.count():
-		# At this point, prev = fibonacci(i - 1) and cur = fibonacci(i)
-		if len(str(cur)) > DIGITS:
-			raise RuntimeError("Not found")
-		elif len(str(cur)) == DIGITS:
-			return str(i)
-		
-		# Advance the Fibonacci sequence by one step
-		prev, cur = cur, prev + cur
+	"""
+	calculates the first fibonacci number to exceed
+	1000 digits using logs.
+	runs in constant time
+	"""
+	golden_ratio=0.5*(1+sqrt(5))
 
+	log_ratio=log10(golden_ratio)
+
+	print(int(1000/log_ratio-2))
 
 if __name__ == "__main__":
 	print(compute())
