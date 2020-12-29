b844e4c84829da0d042d4f8b55373e0e03a106ca
AlkisPis <a_pis@otenet.gr>
2020-12-29 21:20:23 +0200

    Update p032.py
    '''
    The previous solution, once more,  is not transparent, i.e. the process of producing pandigital products is not at all evident, the results  cannot be examined and verified, and  and, most importantly, the total that the code was producing, i.e. 45228, is completely wrong based on what the problem asks, namely "dinding the sum of all products whose multiplicand/multiplier/product identity can be written as a 1 through 9 pandigital." The code below, shows how to obtain these products:
    '''
    `
    def compute():
        # Returns a list with all the divisors of 'n'
        def all_divisors(n):
          return [i for i in range(1,n//2+1) if not n % i]
    
        # Returns True/False if 'n' contains or not duplicate digits
        def contains_duplicates(n):
          s = str(n)
          for i in range(len(s)):
            if s[i] in s[i+1:]: return True
          return False
    
        def in_sn(n):   # Check 1) 'n' is pandigital and 2) if its digits are already in global 'sn'
          global sn   # Return True/False if it's/not a new pandigital 
          s = str(n)  # If False, 'sn' will be updated
          sn2 = sn # Use a temporar var
          for i in range(len(s)):
            if s[i] in sn2: return True
            sn2 += s[i] # Update 'sn' (This is only temporary)
          sn = sn2 # Now it can be updated
          return False
    
        aplist = []  # This is not necessary, but it is used for viewing the reuslts and confirming their validity
        total = 0    # This will carry total solutions
        for n in range(1, 10000):
          sn = str(n) # Current product to be examinded, as string _ It serves to check digit uniqueness
          if not contains_duplicates(sn):
            if not '0' in str(n):   # Exclude 0's from numbers
              lst = all_divisors(n) # Get all divisors of current number
              plist_s = [] # This is only for viewing/checking each result
              sn_bak = sn  # Keep a backup to restore
              for i in range(1, len(lst)): # Exclude '1' from divisors
                n1 = lst[i]            # Fetch next divisor - This will be the multiplicand of the product
                if not '0' in str(n1): # Exclude 0's
                  if not in_sn(n1):    # If not alread in 'sn'
                    n2 = int(n/n1)     # Get the remainder from current 'n' - This will be the multiplier of the product
                    # 'n2' will be valid only if it lies ahead in the divisors list
                    # So we avoid swapping multiplicand with multipliers!
                    if not '0' in str(n2) and n2 > n1 and not in_sn(n2):
                      total += 1; aplist.append(str(n1)+"*"+str(n2)+"="+str(n))
                sn = sn_bak # Restore 'sn' for next round
        print("Total: {}\naplist: {}, {}".format(total, len(aplist), aplist))
    
    `

--- python/p032.py
+++ python/p032.py
@@ -6,27 +6,52 @@
 # https://github.com/nayuki/Project-Euler-solutions
 # 
 
-import eulerlib
-
+# Note: This code isobviously much longer that the one presented previously as a solution,
+#       but the process is "transparent" and, more importantly the results can be examined and verified
 
 def compute():
-	# For contradiction suppose a candidate (x, y, z) has z >= 10000.
-	# Then x*y consumes at least 5 digits. With the 4 (or fewer) remaining digits, even the
-	# upper bound of x=99 and y=99 produces a product of x*y < 10000, which is unequal to z.
-	# Therefore we need the product z < 10000 to be able to find possible x and y values.
-	ans = sum(i for i in range(1, 10000) if has_pandigital_product(i))
-	return str(ans)
+    # Returns a list with all the divisors of 'n'
+    def all_divisors(n):
+      return [i for i in range(1,n//2+1) if not n % i]
 
+    # Returns True/False if 'n' contains or not duplicate digits
+    def contains_duplicates(n):
+      s = str(n)
+      for i in range(len(s)):
+        if s[i] in s[i+1:]: return True
+      return False
 
-def has_pandigital_product(n):
-	# Find and examine all factors of n
-	for i in range(1, eulerlib.sqrt(n) + 1):
-		if n % i == 0:
-			temp = str(n) + str(i) + str(n // i)
-			if "".join(sorted(temp)) == "123456789":
-				return True
-	return False
+    def in_sn(n):   # Check 1) 'n' is pandigital and 2) if its digits are already in global 'sn'
+      global sn   # Return True/False if it's/not a new pandigital 
+      s = str(n)  # If False, 'sn' will be updated
+      sn2 = sn # Use a temporar var
+      for i in range(len(s)):
+        if s[i] in sn2: return True
+        sn2 += s[i] # Update 'sn' (This is only temporary)
+      sn = sn2 # Now it can be updated
+      return False
+
+    aplist = []  # This is not necessary, but it can be  used for examining the results and confirming their validity
+    total = 0    # This will carry total solutions
+    for n in range(1, 10000):
+      sn = str(n) # Current product to be examinded, as string _ It serves to check digit uniqueness
+      if not contains_duplicates(sn):
+        if not '0' in str(n):   # Exclude 0's from numbers
+          lst = all_divisors(n) # Get all divisors of current number
+          plist_s = [] # This is only for viewing/checking each result
+          sn_bak = sn  # Keep a backup to restore
+          for i in range(1, len(lst)): # Exclude '1' from divisors
+            n1 = lst[i]            # Fetch next divisor - This will be the multiplicand of the product
+            if not '0' in str(n1): # Exclude 0's
+              if not in_sn(n1):    # If not alread in 'sn'
+                n2 = int(n/n1)     # Get the remainder from current 'n' - This will be the multiplier of the product
+                # 'n2' will be valid only if it lies ahead in the divisors list
+                # So we avoid swapping multiplicand with multipliers!
+                if not '0' in str(n2) and n2 > n1 and not in_sn(n2):
+                  total += 1; aplist.append(str(n1)+"*"+str(n2)+"="+str(n))
+            sn = sn_bak # Restore 'sn' for next round
+    return total
 
 
 if __name__ == "__main__":
-	print(compute())
+	print(compute()) # = 187
